0f16d8bcbc84426f0f01dbc540bb6ccf1024d8ad
Mike Auty <mike.auty@gmail.com>
2012-11-11 19:02:39 +0000

    Initial volatility3 commit.

---
+++ volatility/interfaces.py
@@ -0,0 +1,33 @@
+"""
+Created on 11 Nov 2012
+
+@author: mike
+"""
+
+class ObjectInterface(object):
+    """
+    classdocs
+    """
+
+    def __init__(self, profileset, offset, data):
+        """
+        Constructor
+        """
+
+    def cast(self):
+
+    # Properties = [size, layer, offset]
+
+
+class TransformLayerInterface(object):
+    """
+    classdocs
+    """
+    
+    def __init__(self):
+        """
+        Constructor
+        """
+        
+    def 
+
