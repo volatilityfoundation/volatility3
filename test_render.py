92894772574ca313586fd6896b30b674df181c37
Mike Auty <mike.auty@gmail.com>
2014-04-14 02:22:20 +0100

    Add in renderer test_rig.

---
+++ test_render.py
@@ -0,0 +1,16 @@
+
+from volatility.framework import renderers
+from volatility.framework.renderers import basic
+
+if __name__ == '__main__':
+
+    grid = renderers.TreeGrid([('offset', int, '#010'),
+                               ('name', str, '20')])
+    grid.add_child(renderers.TreeRow(grid, [10, 'hello']))
+    grid.add_child(renderers.TreeRow(grid, [20, 'hello']))
+    row3 = renderers.TreeRow(grid, [30, 'with_child'])
+    grid.add_child(row3)
+    row3.add_child(renderers.TreeRow(grid, [40, 'child']))
+
+    tr = basic.TextRenderer(None)
+    tr.render(grid)
