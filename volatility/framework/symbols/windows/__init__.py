5655548d3f6fa327710e3566c5f4c2076b633cae
Mike Auty <mike.auty@gmail.com>
2014-12-31 03:46:34 +0000

    Allow owning_process to be called without parameters.

--- volatility/framework/symbols/windows/__init__.py
+++ volatility/framework/symbols/windows/__init__.py
@@ -4,6 +4,6 @@ import volatility.framework.objects as objects
 
 
 class _ETHREAD(objects.Struct):
-    def owning_process(self, kernel_layer):
+    def owning_process(self, kernel_layer = None):
         """Return the EPROCESS that owns this thread"""
         return self.ThreadsProcess.dereference(kernel_layer)
