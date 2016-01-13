d2730fc95f85b3a9c9e65de81493035226fb58de
Mike Auty <mike.auty@gmail.com>
2016-01-13 00:11:50 +0000

    Ensure the swap_layer is optional for intel spaces.

--- volatility/framework/layers/intel.py
+++ volatility/framework/layers/intel.py
@@ -17,7 +17,7 @@ class Intel(interfaces.layers.TranslationLayerInterface):
                 "architecture": "ia32"
                 }
 
-    def __init__(self, context, name, page_map_offset, memory_layer, swap_layer):
+    def __init__(self, context, name, page_map_offset, memory_layer, swap_layer = None):
         interfaces.layers.TranslationLayerInterface.__init__(self, context, name)
         self._base_layer = memory_layer
         self._page_map_offset = page_map_offset
