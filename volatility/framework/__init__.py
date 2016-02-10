d37163d88130c1598386d4951b612e850096c5a4
Mike Auty <mike.auty@gmail.com>
2016-02-10 07:35:04 +0000

    Add in python minimum requirement checks.

--- volatility/framework/__init__.py
+++ volatility/framework/__init__.py
@@ -13,6 +13,7 @@ import inspect
 # 2. If the interface has changed, increment current, set revision to 0
 # 3. If only additions to the interface have been made, increment age
 # 4. If changes or removals of the interface have been made, set age to 0
+import sys
 
 CURRENT = 3  # Number of releases of the library with any change
 REVISION = 0  # Number of changes that don't affect the interface
@@ -28,13 +29,13 @@ def require_version(*args):
     """Checks the required version of a plugin"""
     if len(args):
         if args[0] != version()[0]:
-            raise Exception("Framework version " + str(version()[0]) +
-                            " is incompatible with required version " + str(args[0]))
+            raise RuntimeError("Framework version " + str(version()[0]) +
+                               " is incompatible with required version " + str(args[0]))
         if len(args) > 1:
             if args[1] > version()[1]:
-                raise Exception("Framework version " + ".".join([str(x) for x in version()[0:1]]) +
-                                " is an older revision than the required version " +
-                                ".".join([str(x) for x in args[0:2]]))
+                raise RuntimeError("Framework version " + ".".join([str(x) for x in version()[0:1]]) +
+                                   " is an older revision than the required version " +
+                                   ".".join([str(x) for x in args[0:2]]))
 
 
 def class_subclasses(cls):
@@ -46,5 +47,9 @@ def class_subclasses(cls):
         for return_value in class_subclasses(clazz):
             yield return_value
 
+
+# Check the python version to ensure it's suitable
+if sys.version_info.major != 3 or sys.version_info.minor < 4:
+    raise RuntimeError("Volatility framework requires python version 3.4 or greater")
 
 from volatility.framework import interfaces, symbols, layers, contexts, configuration
