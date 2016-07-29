b63f07f493099db01a39659da27cd0c06d9d1ccb
Mike Auty <mike.auty@gmail.com>
2016-07-29 02:57:10 +0100

    Rework the imports to make the intel layers easier to read.

--- volatility/framework/layers/intel.py
+++ volatility/framework/layers/intel.py
@@ -7,8 +7,8 @@ Created on 7 May 2013
 import math
 import struct
 
-import volatility.framework.configuration.requirements
-from volatility.framework import interfaces, exceptions
+from volatility.framework import exceptions, interfaces
+from volatility.framework.configuration import requirements
 
 
 class Intel(interfaces.layers.TranslationLayerInterface):
@@ -141,16 +141,16 @@ class Intel(interfaces.layers.TranslationLayerInterface):
 
     @classmethod
     def get_requirements(cls):
-        return [volatility.framework.configuration.requirements.TranslationLayerRequirement(name = 'memory_layer',
-                                                                                            constraints = {
-                                                                                                "type": "physical"},
-                                                                                            optional = False),
-                volatility.framework.configuration.requirements.TranslationLayerRequirement(name = 'swap_layer',
-                                                                                            constraints = {
-                                                                                                "type": "physical"},
-                                                                                            optional = True),
-                volatility.framework.configuration.requirements.IntRequirement(name = 'page_map_offset',
-                                                                               optional = False)]
+        return [requirements.TranslationLayerRequirement(name = 'memory_layer',
+                                                         constraints = {
+                                                             "type": "physical"},
+                                                         optional = False),
+                requirements.TranslationLayerRequirement(name = 'swap_layer',
+                                                         constraints = {
+                                                             "type": "physical"},
+                                                         optional = True),
+                requirements.IntRequirement(name = 'page_map_offset',
+                                            optional = False)]
 
 
 class IntelPAE(Intel):
