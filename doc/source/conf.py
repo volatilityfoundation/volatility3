e173c7c080b96457c32db03906c45c0fce98e485
Nick L. Petroni, Jr <npetroni@volexity.com>
2016-08-20 14:12:37 -0400

    Squashed commit of the following:
    commit fbf5eb630919ded28119eaa470d248670b7b78b6
    Author: Mike Auty <mike.auty@gmail.com>
    Date:   Sat Aug 20 13:21:47 2016 +0100
    
        Remove other unnecessary files and drop the templates, but use the static files.
    
    commit 2bf92be628a7047b5f5044c1466ca98c0bf6c8b2
    Author: Mike Auty <mike.auty@gmail.com>
    Date:   Sat Aug 20 13:10:20 2016 +0100
    
        Remove unnecessary elements imported with the python3 doc theme.
    
    commit 31b6d84e58dfc19c63304ffafa3daf03db001e0d
    Author: Mike Auty <mike.auty@gmail.com>
    Date:   Sat Aug 20 12:07:08 2016 +0100
    
        Add in python3 documentation theme files and convert to using those.

--- doc/source/conf.py
+++ doc/source/conf.py
@@ -105,7 +105,10 @@ pygments_style = 'sphinx'
 
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
-html_theme = 'default'
+# html_theme = 'default'
+html_theme = 'pydoctheme'
+html_theme_path = ['tools']
+html_theme_options = {'collapsiblesidebar': True}
 
 # Theme options are theme-specific and customize the look and feel of a theme
 # further.  For a list of options available for each theme, see the
@@ -134,7 +137,7 @@ html_theme = 'default'
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
-html_static_path = ['_static']
+html_static_path = ['_static', 'tools/static']
 
 # Add any extra paths that contain custom files (such as robots.txt or
 # .htaccess) here, relative to this directory. These files are copied
