7f887414cb2be8eecfc0708e6f89e001312d43bd
Mike Auty <mike.auty@gmail.com>
2016-12-29 15:23:35 +0000

    Clean up an unnecessary 'as' import.

--- volatility/plugins/__init__.py
+++ volatility/plugins/__init__.py
@@ -3,6 +3,6 @@
    This is the namespace for all volatility plugins,
    and determines the path for loading plugins
 """
-import volatility.framework.constants as constants
+from volatility.framework import constants
 
 __path__ = constants.PLUGINS_PATH
