d0b1177c317195817df8cfb1b6b6d99686c0d492
Mike Auty <mike.auty@gmail.com>
2017-07-16 18:05:08 +0100

    Add in the linux signature scanner.

--- volatility/framework/automagic/linux.py
+++ volatility/framework/automagic/linux.py
@@ -1,11 +1,77 @@
-from volatility.framework import interfaces
-from volatility.framework.layers import scanners, intel
+import logging
+import os
+import pickle
+
+from volatility.framework import interfaces, constants
+from volatility.framework.layers import intel, scanners
+
+vollog = logging.getLogger(__name__)
+
+
+class LinuxSymbolFinder(interfaces.automagic.AutomagicInterface):
+    priority = 20
+
+    def __init__(self, context, config_path):
+        super().__init__(context, config_path)
+        self._requirements = None
+        self._linux_banners = {}
+        self._load_linux_banners()
+
+    def __call__(self, context, config_path, requirement, progress_callback = None):
+        """Searches for LinuxSymbolRequirements and attempt to populate them"""
+        self._requirements = self.find_requirements(context, config_path, requirement,
+                                                    (interfaces.configuration.TranslationLayerRequirement,
+                                                     interfaces.configuration.SymbolRequirement),
+                                                    shortcut = False)
+
+        for (path, sub_path, requirement) in self._requirements:
+            if isinstance(requirement, interfaces.configuration.SymbolRequirement):
+                for (tl_path, tl_sub_path, tl_requirement) in self._requirements:
+                    # Find the TranslationLayer sibling to the SymbolRequirement
+                    if (isinstance(tl_requirement, interfaces.configuration.TranslationLayerRequirement) and
+                                tl_path == path):
+                        # TODO: Find the physical layer properly, not just for Intel
+                        physical_path = interfaces.configuration.path_join(tl_sub_path, "memory_layer")
+                        self._banner_scan(context, path, requirement, context.config[physical_path],
+                                          progress_callback)
+
+    def _load_linux_banners(self):
+        if os.path.exists(constants.LINUX_BANNERS_PATH):
+            with open(constants.LINUX_BANNERS_PATH, "rb") as f:
+                # We use pickle over JSON because we're dealing with bytes objects
+                self._linux_banners.update(pickle.load(f))
+
+    def _banner_scan(self, context, config_path, requirement, layer_name, progress_callback = None):
+        """Accepts a context, config_path and SymbolRequirement, with a constructed layer_name
+        and scans the layer for linux banners"""
+        mss = scanners.MultiStringScanner(list(self._linux_banners))
+
+        layer = context.memory[layer_name]
+
+        for offset, banner in layer.scan(context = context, scanner = mss, progress_callback = progress_callback):
+            vollog.debug("Identified banner: {}".format(repr(banner)))
+            symbol_files = self._linux_banners[banner]
+            isf_path = "file://" + symbol_files[0]
+            if isf_path:
+                vollog.debug("Using symbol library: {}".format(symbol_files[0]))
+                clazz = "volatility.framework.symbols.linux.LinuxKernelIntermedSymbols"
+                # Set the discovered options
+                path_join = interfaces.configuration.path_join
+                context.config[path_join(config_path, requirement.name, "class")] = clazz
+                context.config[path_join(config_path, requirement.name, "isf_filepath")] = isf_path
+                # Construct the appropriate symbol table
+                requirement.construct(context, config_path)
+                break
+            else:
+                vollog.debug("Symbol library path not found: {}".format(symbol_files[0]))
+                # print("Kernel", banner, hex(banner_offset))
+        else:
+            pass
 
 
 class LintelStacker(interfaces.automagic.StackerLayerInterface):
-    # This signature makes an assumption that Linux is using a Symmetric Multi-Processor
     linux_signature = b"SYMBOL\(swapper_pg_dir\)=.*"
-    stack_order = 40
+    stack_order = 9
 
     @classmethod
     def stack(cls, context, layer_name, progress_callback = None):
@@ -13,6 +79,7 @@ class LintelStacker(interfaces.automagic.StackerLayerInterface):
         layer = context.memory[layer_name]
 
         # Bail out if we're not a physical layer
+        # TODO: We need a better way of doing this
         if isinstance(layer, intel.Intel):
             return None
 
