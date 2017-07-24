8ad9fbfd24e70571f2d9ec5b4893cc14a1ebe9af
Mike Auty <mike.auty@gmail.com>
2017-07-24 15:41:47 +0100

    Ensure imported plugins have their correct requirements fulfilled (even if they change).

--- volatility/plugins/windows/dlllist.py
+++ volatility/plugins/windows/dlllist.py
@@ -1,7 +1,6 @@
 import volatility.framework.interfaces.plugins as plugins
 import volatility.plugins.windows.pslist as pslist
 from volatility.framework import exceptions, renderers
-from volatility.framework.configuration import requirements
 from volatility.framework.renderers import format_hints
 
 
@@ -10,13 +9,8 @@ class DllList(plugins.PluginInterface):
 
     @classmethod
     def get_requirements(cls):
-        return [requirements.TranslationLayerRequirement(name = 'primary',
-                                                         description = 'Kernel Address Space'),
-                requirements.SymbolRequirement(name = "ntkrnlmp",
-                                               description = "Windows OS"),
-                requirements.IntRequirement(name = 'pid',
-                                            description = "Process ID",
-                                            optional = True)]
+        # Since we're calling the plugin, make sure we have the plugin's requirements
+        return pslist.PsList.get_requirements() + []
 
     def _generator(self, procs):
 
