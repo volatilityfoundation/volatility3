334412330ca5d4d026a622c44f0b2259ed535dbc
Mike Auty <mike.auty@gmail.com>
2017-10-09 00:02:03 +0100

    Ensure offsets are appropriately prefixed, given we can now support accepting 0x### numbers.

--- volatility/framework/renderers/text.py
+++ volatility/framework/renderers/text.py
@@ -25,8 +25,8 @@ def hex_bytes_as_text(value):
 
 
 class QuickTextRenderer(interfaces.renderers.Renderer):
-    type_renderers = {format_hints.Bin: lambda x: "{:b}".format(x),
-                      format_hints.Hex: lambda x: "{:x}".format(x),
+    type_renderers = {format_hints.Bin: lambda x: "0b{:b}".format(x),
+                      format_hints.Hex: lambda x: "0x{:x}".format(x),
                       format_hints.HexBytes: hex_bytes_as_text,
                       bytes: lambda x: x.decode("utf-8"),
                       'default': lambda x: "{}".format(x)}
