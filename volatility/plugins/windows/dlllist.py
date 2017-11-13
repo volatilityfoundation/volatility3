330832e546d03f1ce5b57f3df14e83d1c4fc9801
Mike Auty <mike.auty@gmail.com>
2017-11-13 01:20:08 +0000

    Just to make it easier in case of renames, etc.

--- volatility/plugins/windows/dlllist.py
+++ volatility/plugins/windows/dlllist.py
@@ -34,7 +34,7 @@ class DllList(interfaces_plugins.PluginInterface):
 
     def run(self):
 
-        plugin = pslist.PsList(self.context, "plugins.DllList")
+        plugin = pslist.PsList(self.context, self.config_path)
 
         return renderers.TreeGrid([("PID", int),
                                    ("Process", str),
