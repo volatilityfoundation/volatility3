ee12b81f4adf12e3eed32a7402a8ad38e917c822
Mike Auty <mike.auty@gmail.com>
2017-12-10 14:57:26 +0000

    Add in type-annotations for renderers and start on symbols.

--- volatility/framework/symbols/__init__.py
+++ volatility/framework/symbols/__init__.py
@@ -2,6 +2,7 @@ import collections
 import collections.abc
 import enum
 import logging
+import typing
 
 from volatility.framework import constants, exceptions, interfaces, objects, validity
 from volatility.framework.symbols import native, windows, linux
@@ -15,6 +16,12 @@ class SymbolType(enum.Enum):
     ENUM = 3
 
 
+SymbolSpaceReturnType = typing.TypeVar("SymbolSpaceReturnType",
+                                       interfaces.objects.Template,
+                                       interfaces.symbols.Symbol,
+                                       typing.Dict[str, typing.Any])
+
+
 class SymbolSpace(interfaces.symbols.SymbolSpaceInterface, validity.ValidityRoutines):
     """Handles an ordered collection of SymbolTables
 
@@ -22,13 +29,13 @@ class SymbolSpace(interfaces.symbols.SymbolSpaceInterface, validity.ValidityRout
        proceed down through the ranks if a namespace isn't specified.
     """
 
-    def __init__(self):
+    def __init__(self) -> None:
         super().__init__()
-        self._dict = collections.OrderedDict()
+        self._dict = collections.OrderedDict()  # type: typing.Dict[str, interfaces.symbols.BaseSymbolTableInterface]
         # Permanently cache all resolved symbols
-        self._resolved = {}
+        self._resolved = {}  # type: typing.Dict[str, interfaces.objects.Template]
 
-    def free_table_name(self, prefix = "layer"):
+    def free_table_name(self, prefix: str = "layer") -> str:
         """Returns an unused table name to ensure no collision occurs when inserting a symbol table"""
         self._check_type(prefix, str)
 
@@ -39,39 +46,39 @@ class SymbolSpace(interfaces.symbols.SymbolSpaceInterface, validity.ValidityRout
 
     ### Symbol functions
 
-    def get_symbols_by_type(self, type_name):
+    def get_symbols_by_type(self, type_name: str) -> typing.Iterable[str]:
         """Returns all symbols based on the type of the symbol"""
         for table in self._dict.keys():
             for symbol_name in self._dict[table].get_symbols_by_type(type_name):
                 yield table + constants.BANG + symbol_name
 
-    def get_symbols_by_location(self, address, table_name = None):
+    def get_symbols_by_location(self, offset: int, table_name: str = None) -> typing.Iterable[str]:
         """Returns all symbols that exist at a specific relative address"""
-        table_list = self._dict.values()
+        table_list = self._dict.values()  # type: typing.Iterable[interfaces.symbols.BaseSymbolTableInterface]
         if table_name is not None:
             if table_name in self._dict:
                 table_list = [self._dict[table_name]]
             else:
                 table_list = []
         for table in table_list:
-            for symbol_name in self._dict[table].get_symbols_by_location(address = address):
-                yield table + constants.BANG + symbol_name
+            for symbol_name in table.get_symbols_by_location(offset = offset):
+                yield table.name + constants.BANG + symbol_name
 
     ### Space functions
 
-    def __len__(self):
+    def __len__(self) -> int:
         """Returns the number of tables within the space"""
         return len(self._dict)
 
-    def __getitem__(self, i):
+    def __getitem__(self, i: str) -> typing.Any:
         """Returns a specific table from the space"""
         return self._dict[i]
 
-    def __iter__(self):
+    def __iter__(self) -> typing.Iterator[str]:
         """Iterates through all available tables in the symbol space"""
         return iter(self._dict)
 
-    def append(self, value):
+    def append(self, value: interfaces.symbols.BaseSymbolTableInterface) -> None:
         """Adds a symbol_list to the end of the space"""
         if not isinstance(value, interfaces.symbols.BaseSymbolTableInterface):
             raise TypeError(value)
@@ -79,7 +86,7 @@ class SymbolSpace(interfaces.symbols.SymbolSpaceInterface, validity.ValidityRout
             self.remove(value.name)
         self._dict[value.name] = value
 
-    def remove(self, key):
+    def remove(self, key: str) -> None:
         """Removes a named symbol_list from the space"""
         # Reset the resolved list, since we're removing some symbols
         self._resolved = {}
@@ -98,11 +105,11 @@ class SymbolSpace(interfaces.symbols.SymbolSpaceInterface, validity.ValidityRout
            indicate this failure in resolution.
         """
 
-        def __init__(self, type_name = None, **kwargs):
+        def __init__(self, type_name: str = None, **kwargs) -> None:
             vollog.debug("Unresolved reference: {}".format(type_name))
             super().__init__(type_name = type_name, **kwargs)
 
-    def _weak_resolve(self, resolve_type, name):
+    def _weak_resolve(self, resolve_type: SymbolType, name: str) -> SymbolSpaceReturnType:
         """Takes a symbol name and resolves it with ReferentialTemplates"""
         if resolve_type == SymbolType.TYPE:
             get_function = 'get_type'
@@ -120,13 +127,10 @@ class SymbolSpace(interfaces.symbols.SymbolSpaceInterface, validity.ValidityRout
             try:
                 return getattr(self._dict[table_name], get_function)(component_name)
             except KeyError as e:
-                vollog.debug('Type {} references missing Type/Symbol/Enum: {}'.format(name, e))
-                return self._UnresolvedTemplate(name)
-            except exceptions.SymbolError:
-                return self._UnresolvedTemplate(name)
+                raise exceptions.SymbolError('Type {} references missing Type/Symbol/Enum: {}'.format(name, e))
         raise exceptions.SymbolError("Malformed name: {}".format(name))
 
-    def get_type(self, type_name):
+    def get_type(self, type_name: str) -> interfaces.objects.Template:
         """Takes a symbol name and resolves it
 
            This method ensures that all referenced templates (including self-referential templates)
@@ -134,7 +138,7 @@ class SymbolSpace(interfaces.symbols.SymbolSpaceInterface, validity.ValidityRout
         """
         # Traverse down any resolutions
         if type_name not in self._resolved:
-            self._resolved[type_name] = self._weak_resolve(SymbolType.TYPE, type_name)
+            self._resolved[type_name] = self._weak_resolve(SymbolType.TYPE, type_name)  # type: ignore
             traverse_list = [type_name]
             replacements = set()
             # Whole Symbols that still need traversing
@@ -149,8 +153,11 @@ class SymbolSpace(interfaces.symbols.SymbolSpaceInterface, validity.ValidityRout
                             # to the "symbols that still need traversing" list
                             if child.vol.type_name not in self._resolved:
                                 traverse_list.append(child.vol.type_name)
-                                self._resolved[child.vol.type_name] = self._weak_resolve(SymbolType.TYPE,
-                                                                                         child.vol.type_name)
+                                try:
+                                    self._resolved[child.vol.type_name] = self._weak_resolve(SymbolType.TYPE,
+                                                                                             child.vol.type_name)
+                                except exceptions.SymbolError:
+                                    self._resolved[child.vol.type_name] = self._UnresolvedTemplate(child.vol.type_name)
                             # Stash the replacement
                             replacements.add((traverser, child))
                         elif child.children:
@@ -161,18 +168,23 @@ class SymbolSpace(interfaces.symbols.SymbolSpaceInterface, validity.ValidityRout
             raise exceptions.SymbolError("Unresolvable symbol requested: {}".format(type_name))
         return self._resolved[type_name]
 
-    def get_symbol(self, symbol_name):
+    def get_symbol(self, symbol_name: str) -> interfaces.symbols.Symbol:
         """Look-up a symbol name across all the contained symbol spaces"""
-        return self._weak_resolve(SymbolType.SYMBOL, symbol_name)
+        retval = self._weak_resolve(SymbolType.SYMBOL, symbol_name)
+        if not isinstance(retval, interfaces.symbols.Symbol):
+            raise exceptions.SymbolError("Unresolvable Symbol: {}".format(symbol_name))
+        return retval
 
-    def get_enumeration(self, enum_name):
+    def get_enumeration(self, enum_name: str) -> typing.Dict[str, typing.Any]:
         """Look-up a set of enumeration choices from a specific symbol table"""
-        return self._weak_resolve(SymbolType.ENUM, enum_name)
+        retval = self._weak_resolve(SymbolType.ENUM, enum_name)
+        if not isinstance(retval, dict):
+            raise exceptions.SymbolError("Unresolvable Enumeration: {}".format(enum_name))
+        return retval
 
-    def _membership(self, member_type, name):
+    def _membership(self, member_type: SymbolType, name: str) -> bool:
         """Test for membership of a component within a table"""
 
-        table = []
         name_array = name.split(constants.BANG)
         if len(name_array) == 2:
             table_name = name_array[0]
@@ -180,8 +192,9 @@ class SymbolSpace(interfaces.symbols.SymbolSpaceInterface, validity.ValidityRout
         else:
             return False
 
-        if table_name in self:
-            table = self[table_name]
+        if table_name not in self:
+            return False
+        table = self[table_name]
 
         if member_type == SymbolType.TYPE:
             return component_name in table.types
@@ -191,11 +204,11 @@ class SymbolSpace(interfaces.symbols.SymbolSpaceInterface, validity.ValidityRout
             return component_name in table.enumerations
         return False
 
-    def has_type(self, name):
+    def has_type(self, name: str) -> bool:
         return self._membership(SymbolType.TYPE, name)
 
-    def has_symbol(self, name):
+    def has_symbol(self, name: str) -> bool:
         return self._membership(SymbolType.SYMBOL, name)
 
-    def has_enumeration(self, name):
+    def has_enumeration(self, name: str) -> bool:
         return self._membership(SymbolType.ENUM, name)
