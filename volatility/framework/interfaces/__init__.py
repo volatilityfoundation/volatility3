4bf22fbec8d5ff3a89d59b7f4e03f7b3234b6cf1
Mike Auty <mike.auty@gmail.com>
2018-03-20 17:01:18 +0000

    Revert "Reorder the imports for python 3.5.2."
    This reverts commit 3aad2719134fbdc57504e797cac62a487998b621.

--- volatility/framework/interfaces/__init__.py
+++ volatility/framework/interfaces/__init__.py
@@ -7,5 +7,5 @@ and help them understand how to use the internal components of volatility to wri
 # Import the submodules we want people to be able to use without importing them themselves
 # This will also avoid namespace issues, because people can use interfaces.layers to
 # avoid clashing with the layers package
-from volatility.framework.interfaces import context, layers, objects, configuration, \
+from volatility.framework.interfaces import configuration, context, layers, objects, \
     plugins, renderers, symbols, automagic
