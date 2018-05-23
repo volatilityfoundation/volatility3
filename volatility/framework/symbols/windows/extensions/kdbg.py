5aea54df9b56bceb0ce275db818fdb8d62544ab0
Michael Ligh <mligh@volexity.com>
2018-05-23 12:03:27 -0500

    add _KDDEBUGGER_DATA64.get_build_lab() and get_csdversion(). add more properties to wininfo

--- volatility/framework/symbols/windows/extensions/kdbg.py
+++ volatility/framework/symbols/windows/extensions/kdbg.py
@@ -21,3 +21,31 @@ class _KDDEBUGGER_DATA64(objects.Struct):
 
         for proc in eproc.ActiveProcessLinks:
             yield proc
+
+    def get_build_lab(self):
+        """Returns the NT build lab string from the KDBG"""
+
+        layer_name = self.vol.layer_name
+
+        # FIXME: where does this come from?
+        nt_symbol_name = "nt_symbols1"
+
+        kvo = self._context.memory[layer_name].config['kernel_virtual_offset']
+        ntkrnlmp = self._context.module(nt_symbol_name, layer_name=layer_name, offset=kvo)
+
+        return ntkrnlmp.object(type_name="string",
+                               offset=self.NtBuildLab,
+                               max_length=32,
+                               errors="replace")
+
+    def get_csdversion(self):
+        """Returns the CSDVersion as an integer (i.e. Service Pack number)"""
+
+        layer_name = self.vol.layer_name
+        symbol_table_name = self.get_symbol_table().name
+
+        csdresult = self._context.object(symbol_table_name + constants.BANG + "unsigned long",
+                                         layer_name=layer_name,
+                                         offset=self.CmNtCSDVersion)
+
+        return (csdresult >> 8) & 0xffffffff
--- volatility/plugins/windows/wininfo.py
+++ volatility/plugins/windows/wininfo.py
@@ -1,3 +1,4 @@
+import datetime
 import volatility.framework.interfaces.plugins as plugins
 from volatility.framework.configuration import requirements
 from volatility.framework.renderers import TreeGrid
@@ -12,11 +13,24 @@ class WinInfo(plugins.PluginInterface):
                                                          architectures=["Intel32", "Intel64"]),
                 requirements.SymbolRequirement(name="nt_symbols", description="Windows OS")]
 
-    def _generator(self, kdbg):
+    def get_depends(self, layer_name, i=0):
+        """List the dependencies of a given layer.
 
-        yield (0, ("Foo", "Bar"))
+        :param layer_name: the name of the starting layer
+        :param i: the index/order of the layer
+        """
+        layer = self.context.memory[layer_name]
+        yield i, layer
+        try:
+            for depends in layer.dependencies:
+                for j, dep in self.get_depends(depends, i + 1):
+                    yield j, self.context.memory[dep.name]
+        except AttributeError:
+            # FileLayer won't have dependencies
+            pass
 
-    def run(self):
+    def _generator(self):
+
         virtual_layer_name = self.config["primary"]
         virtual_layer = self.context.memory[virtual_layer_name]
 
@@ -26,20 +40,68 @@ class WinInfo(plugins.PluginInterface):
                                                      "kdbg")
 
         kvo = virtual_layer.config["kernel_virtual_offset"]
-        ntkrnlmp = self.context.module(self.config["nt_symbols"], layer_name=virtual_layer_name, offset=kvo)
+
+        ntkrnlmp = self.context.module(self.config["nt_symbols"],
+                                       layer_name=virtual_layer_name, offset=kvo)
 
         kdbg_offset = ntkrnlmp.get_symbol("KdDebuggerDataBlock").address
 
         kdbg = self.context.object(kdbg_table_name + constants.BANG +
-                            "_KDDEBUGGER_DATA64", offset=kvo + kdbg_offset,
-                            layer_name=virtual_layer_name)
+                                   "_KDDEBUGGER_DATA64", offset=kvo + kdbg_offset,
+                                   layer_name=virtual_layer_name)
 
-        for proc in kdbg.get_processes():
-            print(proc)
+        yield (0, ("Kernel Base", hex(self.config["primary.kernel_virtual_offset"])))
+        yield (0, ("DTB", hex(self.config["primary.page_map_offset"])))
+        yield (0, ("Symbols", self.config["nt_symbols.isf_url"]))
 
-        #vers_addr = kvo + self.context.symbol_space.get_symbol("ntkrnlmp!KdVersionBlock").address
-        #vers = self.context.object("ntkrnlmp!_DBGKD_GET_VERSION64", layer_name=virtual, offset=vers_addr)
+        for i, layer in self.get_depends("primary"):
+            yield (0, (layer.name, "{} {}".format(i, layer.__class__.__name__)))
+
+        yield (0, ("KdDebuggerDataBlock", hex(kdbg.vol.offset)))
+        yield (0, ("NTBuildLab", kdbg.get_build_lab()))
+        yield (0, ("CSDVersion", str(kdbg.get_csdversion())))
+
+        vers_offset = ntkrnlmp.get_symbol("KdVersionBlock").address
+
+        vers = ntkrnlmp.object(type_name="_DBGKD_GET_VERSION64",
+                               layer_name=virtual_layer_name,
+                               offset=kvo + vers_offset)
+
+        yield (0, ("KdVersionBlock", hex(vers.vol.offset)))
+        yield (0, ("Major/Minor", "{0}.{1}".format(vers.MajorVersion, vers.MinorVersion)))
+        yield (0, ("MachineType", str(vers.MachineType)))
+
+        cpu_count_offset = ntkrnlmp.get_symbol("KeNumberProcessors").address
+
+        cpu_count = ntkrnlmp.object(type_name="unsigned int",
+                                    layer_name=virtual_layer_name,
+                                    offset=kvo + cpu_count_offset)
+
+        yield (0, ("KeNumberProcessors", str(cpu_count)))
+
+        # this is a hard-coded address in the Windows OS
+        if virtual_layer.bits_per_register == 32:
+            kuser_addr = 0xFFDF0000
+        else:
+            kuser_addr = 0xFFFFF78000000000
+
+        kuser = ntkrnlmp.object(type_name="_KUSER_SHARED_DATA",
+                                layer_name=virtual_layer_name,
+                                offset=kuser_addr)
+
+        yield (0, ("SystemTime", str(kuser.SystemTime.get_time())))
+        yield (0, ("NtSystemRoot", str(kuser.NtSystemRoot.cast("string", encoding="utf-16", errors="replace", max_length=260))))
+        yield (0, ("NtProductType", str(kuser.NtProductType.description)))
+        yield (0, ("NtMajorVersion", str(kuser.NtMajorVersion)))
+        yield (0, ("NtMinorVersion", str(kuser.NtMinorVersion)))
+        yield (0, ("KdDebuggerEnabled", "True" if ord(kuser.KdDebuggerEnabled) else "False"))
+        yield (0, ("SafeBootMode", "True" if ord(kuser.SafeBootMode) else "False"))
+
+    def run(self):
+
+        #for proc in kdbg.get_processes():
+        #   print(proc)
 
         return TreeGrid([("Variable", str),
                          ("Value", str)],
-                        self._generator(kdbg))
+                        self._generator())
