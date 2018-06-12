a61efebe1e21e30fa615316225aa61d5ae3c1cc5
Michael Ligh <mligh@volexity.com>
2018-06-11 21:23:16 -0500

    use *args and **kwargs when inheriting from IntermediateSymbolTable

--- volatility/framework/symbols/windows/pe.py
+++ volatility/framework/symbols/windows/pe.py
@@ -7,13 +7,8 @@ from volatility.framework.symbols.windows.extensions import pe
 
 class PEIntermedSymbols(intermed.IntermediateSymbolTable):
 
-    def __init__(self,
-                 context: interfaces.context.ContextInterface,
-                 config_path: str,
-                 name: str,
-                 isf_url: str,
-                 table_mapping: typing.Optional[typing.Dict[str, str]] = None) -> None:
-        super().__init__(context = context, config_path = config_path, name = name, isf_url = isf_url, table_mapping = table_mapping)
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
 
         self.set_type_class('_IMAGE_DOS_HEADER', pe._IMAGE_DOS_HEADER)
         # the 32- and 64-bit extensions behave the same way, but the underlying structure is different
