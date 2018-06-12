ae9d7dbc864a5642757973457d818c1961ede962
Michael Ligh <mligh@volexity.com>
2018-06-11 21:23:16 -0500

    use *args and **kwargs when inheriting from IntermediateSymbolTable

--- volatility/framework/symbols/windows/kdbg.py
+++ volatility/framework/symbols/windows/kdbg.py
@@ -6,15 +6,7 @@ from volatility.framework.symbols.windows.extensions import kdbg
 
 class KdbgIntermedSymbols(intermed.IntermediateSymbolTable):
 
-    def __init__(self,
-                 context: interfaces.context.ContextInterface,
-                 config_path: str,
-                 name: str,
-                 isf_url: str,
-                 table_mapping: typing.Optional[typing.Dict[str, str]] = None) -> None:
-        super().__init__(context = context, config_path = config_path, name = name, isf_url = isf_url, table_mapping = table_mapping)
-
-        if table_mapping is None or "nt_symbols" not in table_mapping:
-            raise exceptions.SymbolSpaceError("KdbgIntermedSymbols must be passed a table_mapping with nt_symbols")
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
 
         self.set_type_class('_KDDEBUGGER_DATA64', kdbg._KDDEBUGGER_DATA64)
