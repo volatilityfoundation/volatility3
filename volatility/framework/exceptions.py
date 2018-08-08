19bdc367bfc5884fca1f0ba577c209e4a17d38d1
Mike Auty <mike.auty@gmail.com>
2018-08-08 21:26:25 +0100

    Add in a new exception for when expected data structures cannot be found.

--- volatility/framework/exceptions.py
+++ volatility/framework/exceptions.py
@@ -79,3 +79,7 @@ class LayerException(VolatilityException):
 
 class StructureException(VolatilityException):
     """Thrown when an error occurs dealing with an expected structure type"""
+
+
+class MissingStructureException(VolatilityException):
+    """Thrown when an error occurs due to an expected structure not being present"""
