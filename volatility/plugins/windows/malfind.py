ec3c6f34b839bf7286013d1e3f8ddf5d18551d11
Mike Auty <mike.auty@gmail.com>
2018-08-17 11:03:49 +0100

    Convert malfind to classmethod convention (and fix a broken protect_values call).

--- volatility/plugins/windows/malfind.py
+++ volatility/plugins/windows/malfind.py
@@ -43,7 +43,8 @@ class Malfind(interfaces_plugins.PluginInterface):
 
         return True
 
-    def list_injections(self, vadinfo_plugin, proc):
+    @classmethod
+    def list_injections(cls, context, symbol_table, vadinfo_plugin, proc):
         """Generate memory regions for a process that may contain
         injected code.
 
@@ -53,12 +54,12 @@ class Malfind(interfaces_plugins.PluginInterface):
         """
 
         proc_layer_name = proc.add_process_layer()
-        proc_layer = self.context.memory[proc_layer_name]
+        proc_layer = context.memory[proc_layer_name]
 
         for vad in proc.get_vad_root().traverse():
-            protection_string = vad.get_protection(vadinfo_plugin.protect_values(self.context,
+            protection_string = vad.get_protection(vadinfo_plugin.protect_values(context,
                                                                                  proc_layer_name,
-                                                                                 self.config["nt_symbols"]),
+                                                                                 symbol_table),
                                                    vadinfo.winnt_protections)
             write_exec = "EXECUTE" in protection_string and "WRITE" in protection_string
 
@@ -68,7 +69,7 @@ class Malfind(interfaces_plugins.PluginInterface):
 
             if (vad.get_private_memory() == 1 and vad.get_tag() == "VadS") or (
                     vad.get_private_memory() == 0 and protection_string != "PAGE_EXECUTE_WRITECOPY"):
-                if self.is_vad_empty(proc_layer, vad):
+                if cls.is_vad_empty(proc_layer, vad):
                     continue
 
                 data = proc_layer.read(vad.get_start(), 64, pad = True)
@@ -86,7 +87,7 @@ class Malfind(interfaces_plugins.PluginInterface):
         for proc in procs:
             process_name = utility.array_to_string(proc.ImageFileName)
 
-            for vad, data in self.list_injections(vadinfo_plugin, proc):
+            for vad, data in self.list_injections(self.context, self.config["nt_symbols"], vadinfo_plugin, proc):
 
                 # if we're on a 64 bit kernel, we may still need 32 bit disasm due to wow64
                 if is_32bit_arch or proc.get_is_wow64():
@@ -101,7 +102,10 @@ class Malfind(interfaces_plugins.PluginInterface):
                            format_hints.Hex(vad.get_start()),
                            format_hints.Hex(vad.get_end()),
                            vad.get_tag(),
-                           vad.get_protection(vadinfo_plugin.protect_values(), vadinfo.winnt_protections),
+                           vad.get_protection(vadinfo_plugin.protect_values(self.context,
+                                                                            proc.vol.layer_name,
+                                                                            self.config["nt_symbols"]),
+                                              vadinfo.winnt_protections),
                            vad.get_commit_charge(),
                            vad.get_private_memory(),
                            format_hints.HexBytes(data),
