da6d211a407eb831b4e54ff9fb3283055414c405
Mike Auty <mike.auty@gmail.com>
2018-10-03 21:32:31 +0100

    The isf_url parameter is never populated, so this can go away.

--- volatility/framework/symbols/windows/pe.py
+++ volatility/framework/symbols/windows/pe.py
@@ -1,9 +1,6 @@
-import typing
-
-from volatility.framework import interfaces
-from volatility.framework.configuration import requirements
 from volatility.framework.symbols import intermed
 from volatility.framework.symbols.windows.extensions import pe
+
 
 class PEIntermedSymbols(intermed.IntermediateSymbolTable):
 
@@ -14,8 +11,3 @@ class PEIntermedSymbols(intermed.IntermediateSymbolTable):
         # the 32- and 64-bit extensions behave the same way, but the underlying structure is different
         self.set_type_class('_IMAGE_NT_HEADERS', pe._IMAGE_NT_HEADERS)
         self.set_type_class('_IMAGE_NT_HEADERS64', pe._IMAGE_NT_HEADERS)
-
-    @classmethod
-    def get_requirements(cls) -> typing.List[interfaces.configuration.RequirementInterface]:
-        return [requirements.StringRequirement("isf_url",
-                                               description="JSON file containing the symbols encoded in the Intermediate Symbol Format")]
