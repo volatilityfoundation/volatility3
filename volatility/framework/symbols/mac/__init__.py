1341925160daee2453f038486fe6c4348f2f648e
Mike Auty <mike.auty@gmail.com>
2018-12-13 01:41:07 +0000

    Remove unnecessary requirements method.

--- volatility/framework/symbols/mac/__init__.py
+++ volatility/framework/symbols/mac/__init__.py
@@ -1,9 +1,7 @@
-import typing
-
 from volatility.framework import interfaces
-from volatility.framework.configuration import requirements
 from volatility.framework.symbols import intermed
 from volatility.framework.symbols.mac import extensions
+
 
 class MacKernelIntermedSymbols(intermed.IntermediateSymbolTable):
     provides = {"type": "interface"}
@@ -15,11 +13,6 @@ class MacKernelIntermedSymbols(intermed.IntermediateSymbolTable):
                  isf_url: str) -> None:
         super().__init__(context = context, config_path = config_path, name = name, isf_url = isf_url)
 
-        self.set_type_class('proc',     extensions.proc)
-        #self.set_type_class('fileglob', extensions.fileglob)
-        #self.set_type_class('vnode',    extensions.vnode)
-
-    @classmethod
-    def get_requirements(cls) -> typing.List[interfaces.configuration.RequirementInterface]:
-        return [requirements.StringRequirement("isf_url",
-                                               description = "JSON file containing the symbols encoded in the Intermediate Symbol Format")]
+        self.set_type_class('proc', extensions.proc)
+        # self.set_type_class('fileglob', extensions.fileglob)
+        # self.set_type_class('vnode',    extensions.vnode)
