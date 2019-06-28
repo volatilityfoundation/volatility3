17a0e1eac8144c929047448d673c552b8715366b
Mike Auty <mike.auty@gmail.com>
2019-06-28 15:29:40 +0100

    IntFlag is in python-3.5 by default, it's just if the enum34 package is installed that problems happen.

--- volatility/framework/plugins/windows/poolscanner.py
+++ volatility/framework/plugins/windows/poolscanner.py
@@ -35,8 +35,7 @@ from volatility.framework.symbols.windows import extensions
 vollog = logging.getLogger(__name__)
 
 
-# TODO: When python3.5 is no longer supported, make this enum.IntFlag
-class PoolType(enum.IntEnum):
+class PoolType(enum.IntFlag):
     """Class to maintain the different possible PoolTypes
     The values must be integer powers of 2"""
 
@@ -260,9 +259,9 @@ class PoolScanner(plugins.PluginInterface):
             # registry hives
             PoolConstraint(
                 b'CM10',
-                type_name=symbol_table + constants.BANG + "_CMHIVE",
-                size=(800, None),
-                page_type=PoolType.PAGED | PoolType.NONPAGED | PoolType.FREE),
+                type_name = symbol_table + constants.BANG + "_CMHIVE",
+                size = (800, None),
+                page_type = PoolType.PAGED | PoolType.NONPAGED | PoolType.FREE),
         ]
 
         if not tags_filter:
