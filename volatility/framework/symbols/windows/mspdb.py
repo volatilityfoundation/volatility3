202ff16eacf59f615e5c4dbf9195440f2091d6e6
Mike Auty <mike.auty@gmail.com>
2019-07-07 00:33:09 +0100

    Add support for more base_types, enum and arrays.

--- volatility/framework/symbols/windows/mspdb.py
+++ volatility/framework/symbols/windows/mspdb.py
@@ -1,10 +1,217 @@
 import argparse
 import os
-from typing import Tuple, Dict
+from typing import Tuple, Dict, Any, Optional
 from urllib import request
 
 from volatility.framework import contexts, interfaces
 from volatility.framework.layers import physical, msf
+
+primatives = {
+    0x03: ("void", {
+        "endian": "little",
+        "kind": "void",
+        "signed": True,
+        "size": 0
+    }),
+    # 0x08: ("HRESULT", {}),
+    0x10: ("char", {
+        "endian": "little",
+        "kind": "char",
+        "signed": True,
+        "size": 1
+    }),
+    0x20: ("uchar", {
+        "endian": "little",
+        "kind": "char",
+        "signed": False,
+        "size": 1
+    }),
+    0x68: ("int8", {
+        "endian": "little",
+        "kind": "int",
+        "signed": True,
+        "size": 1
+    }),
+    0x69: ("uint8", {
+        "endian": "little",
+        "kind": "int",
+        "signed": False,
+        "size": 1
+    }),
+    0x70: ("rchar", {
+        "endian": "little",
+        "kind": "char",
+        "signed": False,
+        "size": 1
+    }),
+    0x71: ("wchar", {
+        "endian": "little",
+        "kind": "int",
+        "signed": False,
+        "size": 2
+    }),
+    # 0x7a: ("rchar16", {}),
+    # 0x7b: ("rchar32", {}),
+    0x11: ("int16", {
+        "endian": "little",
+        "kind": "int",
+        "signed": True,
+        "size": 2
+    }),
+    0x21: ("uint16", {
+        "endian": "little",
+        "kind": "int",
+        "signed": False,
+        "size": 2
+    }),
+    0x72: ("int16", {
+        "endian": "little",
+        "kind": "int",
+        "signed": True,
+        "size": 2
+    }),
+    0x73: ("uint16", {
+        "endian": "little",
+        "kind": "int",
+        "signed": False,
+        "size": 2
+    }),
+    0x12: ("int32", {
+        "endian": "little",
+        "kind": "int",
+        "signed": True,
+        "size": 4
+    }),
+    0x22: ("uint32", {
+        "endian": "little",
+        "kind": "int",
+        "signed": False,
+        "size": 4
+    }),
+    0x74: ("int32", {
+        "endian": "little",
+        "kind": "int",
+        "signed": True,
+        "size": 4
+    }),
+    0x75: ("uint32", {
+        "endian": "little",
+        "kind": "int",
+        "signed": False,
+        "size": 2
+    }),
+    0x13: ("int64", {
+        "endian": "little",
+        "kind": "int",
+        "signed": True,
+        "size": 8
+    }),
+    0x23: ("uint64", {
+        "endian": "little",
+        "kind": "int",
+        "signed": False,
+        "size": 8
+    }),
+    0x76: ("int64", {
+        "endian": "little",
+        "kind": "int",
+        "signed": True,
+        "size": 8
+    }),
+    0x77: ("uint64", {
+        "endian": "little",
+        "kind": "int",
+        "signed": False,
+        "size": 8
+    }),
+    0x14: ("int128", {
+        "endian": "little",
+        "kind": "int",
+        "signed": True,
+        "size": 16
+    }),
+    0x24: ("uint128", {
+        "endian": "little",
+        "kind": "int",
+        "signed": False,
+        "size": 16
+    }),
+    0x78: ("int128", {
+        "endian": "little",
+        "kind": "int",
+        "signed": True,
+        "size": 16
+    }),
+    0x79: ("uint128", {
+        "endian": "little",
+        "kind": "int",
+        "signed": False,
+        "size": 16
+    }),
+    0x46: ("f16", {
+        "endian": "little",
+        "kind": "float",
+        "signed": False,
+        "size": 2
+    }),
+    0x40: ("f32", {
+        "endian": "little",
+        "kind": "float",
+        "signed": False,
+        "size": 4
+    }),
+    0x45: ("f32pp", {
+        "endian": "little",
+        "kind": "float",
+        "signed": False,
+        "size": 4
+    }),
+    0x44: ("f48", {
+        "endian": "little",
+        "kind": "float",
+        "signed": False,
+        "size": 6
+    }),
+    0x41: ("f64", {
+        "endian": "little",
+        "kind": "float",
+        "signed": False,
+        "size": 8
+    }),
+    0x42: ("f80", {
+        "endian": "little",
+        "kind": "float",
+        "signed": False,
+        "size": 10
+    }),
+    0x43: ("f128", {
+        "endian": "little",
+        "kind": "float",
+        "signed": False,
+        "size": 16
+    })
+}
+
+indirections = {
+    0x100: ("pointer16", {
+        "endian": "little",
+        "kind": "int",
+        "signed": False,
+        "size": 2
+    }),
+    0x400: ("pointer", {
+        "endian": "little",
+        "kind": "int",
+        "signed": False,
+        "size": 4
+    }),
+    0x600: ("pointer64", {
+        "endian": "little",
+        "kind": "int",
+        "signed": False,
+        "size": 8
+    })
+}
 
 
 class PdbReader:
@@ -69,7 +276,10 @@ class PdbReader:
             raise ValueError("Maximum TPI index is smaller than minimum TPI index, found: {} < {} ".format(
                 header.index_max, header.index_min))
 
+        # Reset the state
         self.types = []
+        self.bases = {}
+        self.user_types = {}
 
         offset = header.header_size
         # Ensure we use the same type everywhere
@@ -90,12 +300,54 @@ class PdbReader:
         if tpi_layer.maximum_address - offset != 0:
             raise ValueError("Type values did not fill the TPI stream correctly")
 
+        for index in range(len(self.types)):
+            if isinstance(self.types[index], Tuple):
+                leaf_type, name, value = self.types[index]
+                if leaf_type in [
+                        leaf_type.LF_CLASS, leaf_type.LF_CLASS_ST, leaf_type.LF_STRUCTURE, leaf_type.LF_STRUCTURE_ST,
+                        leaf_type.LF_INTERFACE
+                ]:
+                    if not value.properties.forward_reference:
+                        self.user_types[name] = {
+                            "kind": "struct",
+                            "size": 0,
+                            "fields": self.convert_fields(value.fields - 0x1000)
+                        }
+
+        print(self.user_types)
+
         return header
 
-    def consume_type(self, module: interfaces.context.ModuleInterface, offset: int, length: int,
-                     no_output = False) -> Tuple[Dict[str, Dict], int]:
-        """Returns the dictionary for the type, and the number of bytes consumed"""
-        result = {}
+    def get_type_from_index(self, index: int) -> Dict[str, Any]:
+        """Takes a type index and returns appropriate dictionary"""
+        if index < 0x1000:
+            base_name, base = primatives[index & 0xff]
+            self.bases[base_name] = base
+            result = {"kind": "base", "name": base_name}
+            indirection = (index & 0xf00)
+            if indirection:
+                pointer_name, pointer_base = indirections[indirection]
+                self.bases[pointer_name] = pointer_base
+                result = {"type": pointer_name, "subtype": result}
+            return result
+        else:
+            leaf_type, name, value = self.types[index - 0x1000]
+            result = {"kind": "struct", "name": name}
+            if leaf_type in [leaf_type.LF_MODIFIER]:
+                result = self.get_type_from_index(value.subtype_index)
+            elif leaf_type in [leaf_type.LF_ARRAY, leaf_type.LF_ARRAY_ST, leaf_type.LF_STRIDED_ARRAY]:
+                pass
+            elif not name:
+                import pdb
+                pdb.set_trace()
+            return result
+
+    def consume_type(
+            self, module: interfaces.context.ModuleInterface, offset: int, length: int
+    ) -> Tuple[Tuple[Optional[interfaces.objects.ObjectInterface], Optional[str], Optional[interfaces.objects.
+                                                                                           ObjectInterface]], int]:
+        """Returns a (leaf_type, name, object) Tuple for a type, and the number of bytes consumed"""
+        result = None, None, None
         leaf_type = self.context.object(
             module.get_enumeration("LEAF_TYPE"), layer_name = module._layer_name, offset = offset)
         consumed = leaf_type.vol.base_type.size
@@ -108,40 +360,45 @@ class PdbReader:
             structure = module.object(type_name = "LF_STRUCTURE", offset = offset)
             name = self.parse_string(leaf_type, structure.name, size = length - structure.vol.size - consumed)
             consumed = length
-            result[name] = structure
+            result = leaf_type, name, structure
         elif leaf_type in [leaf_type.LF_MEMBER, leaf_type.LF_MEMBER_ST]:
             member = module.object(type_name = "LF_MEMBER", offset = offset)
             name = self.parse_string(leaf_type, member.name, size = length - member.vol.size - consumed)
-            result[name] = member
-            print(name)
+            result = leaf_type, name, member
             consumed += member.vol.size + len(name) + 1
         elif leaf_type in [leaf_type.LF_MODIFIER, leaf_type.LF_POINTER, leaf_type.LF_PROCEDURE]:
             # TODO: Subresolve sub-types
             obj = module.object(type_name = leaf_type.lookup(), offset = offset)
+            result = leaf_type, None, obj
             consumed = length
         elif leaf_type in [leaf_type.LF_FIELDLIST]:
             sub_length = length - consumed
             sub_offset = offset
             fields = []
             while length > consumed:
-                subfield, sub_consumed = self.consume_type(module, sub_offset, sub_length, True)
+                subfield, sub_consumed = self.consume_type(module, sub_offset, sub_length)
                 sub_consumed += self.consume_padding(module.layer_name, sub_offset + sub_consumed)
                 sub_length -= sub_consumed
                 sub_offset += sub_consumed
                 consumed += sub_consumed
                 fields.append(subfield)
-            result = fields
+            result = leaf_type, None, fields
         elif leaf_type in [leaf_type.LF_BITFIELD]:
+            result = leaf_type, None, None
             consumed = length
         elif leaf_type in [leaf_type.LF_ARRAY, leaf_type.LF_ARRAY_ST, leaf_type.LF_STRIDED_ARRAY]:
+            array = module.object(type_name = "LF_ARRAY", offset = offset)
+            name = self.parse_string(leaf_type, array.name, size = length - array.vol.size - consumed)
+            result = leaf_type, name, array
             consumed = length
         elif leaf_type in [leaf_type.LF_ARGLIST, leaf_type.LF_ENUMERATE, leaf_type.LF_ENUM, leaf_type.LF_UNION]:
+            enum = module.object(type_name = "LF_ENUM", offset = offset)
+            name = self.parse_string(leaf_type, enum.name, size = length - enum.vol.size - consumed)
+            result = leaf_type, name, enum
             consumed = length
         else:
             raise ValueError("Unhandled leaf_type: {}".format(leaf_type))
 
-        if not no_output:
-            print(leaf_type.lookup())
         return result, consumed
 
     def consume_padding(self, layer_name: str, offset: int) -> int:
@@ -162,6 +419,15 @@ class PdbReader:
             name = structure.cast("pascal_string")
             name = name.string.cast("string", max_length = name.length, encoding = "latin-1")
         return name
+
+    def convert_fields(self, fields: int):
+        result = {}
+        print(fields)
+        _, _, fields_struct = self.types[fields]
+        for field in fields_struct:
+            _, name, member = field
+            result[name] = {"offset": member.offset, "type": self.get_type_from_index(member.field_type)}
+        return result
 
 
 if __name__ == '__main__':
@@ -184,4 +450,3 @@ if __name__ == '__main__':
     import pdb
 
     pdb.set_trace()
-    print(reader.types)
