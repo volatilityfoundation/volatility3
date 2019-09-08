volatilityfoundation/volatility3

commit 5362e2094edbe2fce8b9256577a0753e175b9dcb

Initial attempt at cleaning up the API
Context.object accepts a template or a string name (and now a type
flag).  Module.object only accepts a string (because a template already
has most of the stuff built in and might as well be passed to the
Context.object constructor).

The gotcha here is the absolute flag, which must now be set
appropriately in all cases *except* where the module is constructed
with an offset of 0 (whereby it will have no impact).

volatility/framework/plugins/windows/handles.py
@@ -162,7 +162,7 @@ class Handles(interfaces_plugins.PluginInterface):
             table_addr = ntkrnlmp.get_symbol("ObpObjectTypes").address
 
         ptrs = ntkrnlmp.object(
-            type_name = "array", offset = kvo + table_addr, subtype = ntkrnlmp.get_type("pointer"), count = 100)
+            symbol = "array", offset = table_addr, subtype = ntkrnlmp.get_type("pointer"), count = 100)
 
         for i, ptr in enumerate(ptrs):  #type: ignore
             # the first entry in the table is always null. break the
@@ -214,7 +214,7 @@ class Handles(interfaces_plugins.PluginInterface):
         if not self.context.layers[virtual].is_valid(offset):
             return
 
-        table = ntkrnlmp.object(type_name = "array", offset = offset, subtype = subtype, count = int(count))
+        table = ntkrnlmp.object(symbol = "array", offset = offset, subtype = subtype, count = int(count))
 
         layer_object = self.context.layers[virtual]
         masked_offset = (offset & layer_object.maximum_address)

volatility/cli/volshell/windows.py
@@ -44,7 +44,7 @@ class Volshell(shellplugin.Volshell):
         ntkrnlmp = self.context.module(self.config['nt_symbols'], layer_name = layer_name, offset = kvo)
 
         ps_aph_offset = ntkrnlmp.get_symbol("PsActiveProcessHead").address
-        list_entry = ntkrnlmp.object(type_name = "_LIST_ENTRY", offset = kvo + ps_aph_offset)
+        list_entry = ntkrnlmp.object(symbol = "_LIST_ENTRY", offset = ps_aph_offset)
 
         # This is example code to demonstrate how to use symbol_space directly, rather than through a module:
         #
@@ -58,7 +58,7 @@ class Volshell(shellplugin.Volshell):
         # having been present.  Strictly, the value of the requirement should be joined with the BANG character
         # defined in the constants file
         reloff = ntkrnlmp.get_type("_EPROCESS").relative_child_offset("ActiveProcessLinks")
-        eproc = ntkrnlmp.object(type_name = "_EPROCESS", offset = list_entry.vol.offset - reloff)
+        eproc = ntkrnlmp.object(symbol = "_EPROCESS", offset = list_entry.vol.offset - reloff, absolute = True)
 
         for proc in eproc.ActiveProcessLinks:
             yield proc

volatility/framework/automagic/linux.py
@@ -303,7 +303,7 @@ class LinuxUtilities(object):
                 progress_callback = progress_callback):
             task_symbol = module.get_type('task_struct')
             init_task_address = offset - task_symbol.relative_child_offset('comm')
-            init_task = module.object(type_name = 'task_struct', offset = init_task_address)
+            init_task = module.object(symbol = 'task_struct', offset = init_task_address, absolute = True)
             if init_task.pid != 0:
                 continue
             elif init_task.has_member('state') and init_task.state.cast('unsigned int') != 0:

volatility/framework/constants/__init__.py
@@ -21,6 +21,7 @@
 
 Stores all the constant values that are generally fixed throughout volatility
 This includes default scanning block sizes, etc."""
+import enum
 import os.path
 import sys
 from typing import Optional, Callable
@@ -61,3 +62,9 @@ PARALLELISM_THREADING = 1
 PARALLELISM_MULTIPROCESSING = 2
 
 PARALLELISM = PARALLELISM_OFF
+
+
+class SymbolType(enum.Enum):
+    TYPE = 1
+    SYMBOL = 2
+    ENUM = 3

volatility/framework/contexts/__init__.py
@@ -93,6 +93,7 @@ class Context(interfaces.context.ContextInterface):
                symbol: Union[str, interfaces.objects.Template],
                layer_name: str,
                offset: int,
+               symbol_type: Optional[constants.SymbolType] = None,
                native_layer_name: Optional[str] = None,
                **arguments) -> interfaces.objects.ObjectInterface:
         """Object factory, takes a context, symbol, offset and optional layername
@@ -110,11 +111,20 @@ class Context(interfaces.context.ContextInterface):
             A fully constructed object
         """
         if not isinstance(symbol, interfaces.objects.Template):
-            object_template = self._symbol_space.get_type(symbol)
+            if symbol_type == constants.SymbolType.SYMBOL:
+                symbol_obj = self._symbol_space.get_symbol(symbol)
+                if symbol_obj.type is None:
+                    raise ValueError("Symbol {} has no associated type information".format(symbol_obj.name))
+                object_template = symbol_obj.type
+            elif symbol_type == constants.SymbolType.ENUM:
+                object_template = self._symbol_space.get_enumeration(symbol)
+            else:
+                object_template = self._symbol_space.get_type(symbol)
         else:
             object_template = symbol
             # Ensure that if a pre-constructed type is provided we just instantiate it
             arguments.update(object_template.vol)
+
         object_template = object_template.clone()
         object_template.update_vol(**arguments)
         return object_template(
@@ -149,9 +159,11 @@ def get_module_wrapper(method: str) -> Callable:
     """Returns a symbol using the symbol_table_name of the Module"""
 
     def wrapper(self, name: str) -> Callable:
-        if constants.BANG in name:
-            raise ValueError("Name cannot reference another module")
-        return getattr(self._context.symbol_space, method)(self._module_name + constants.BANG + name)
+        if constants.BANG not in name:
+            name = self._module_name + constants.BANG + name
+        else:
+            raise ValueError("Cannot reference another module when calling {}".format(method))
+        return getattr(self._context.symbol_space, method)(name)
 
     for entry in ['__annotations__', '__doc__', '__module__', '__name__', '__qualname__']:
         proxy_interface = getattr(interfaces.context.ModuleInterface, method)
@@ -164,40 +176,45 @@ def get_module_wrapper(method: str) -> Callable:
 class Module(interfaces.context.ModuleInterface):
 
     def object(self,
-               symbol_name: Optional[str] = None,
-               type_name: Optional[str] = None,
-               offset: Optional[int] = None,
+               symbol: str,
+               symbol_type: Optional[constants.SymbolType] = None,
+               offset: int = None,
                native_layer_name: Optional[str] = None,
-               **kwargs) -> interfaces.objects.ObjectInterface:
+               absolute: bool = False,
+               **kwargs) -> 'interfaces.objects.ObjectInterface':
         """Returns an object created using the symbol_table_name and layer_name of the Module
 
         Args:
-            symbol_name: Name of the symbol (within the module) to construct, type_name and offset must not be specified
-            type_name: Name of the type (within the module) to construct, offset must be specified and symbol_name must not
-            offset: The location (absolute within memory), type_name must be specified and symbol_name must not
+            symbol: Name of the type/symbol/enumeration (within the module) to construct
+            symbol_type: One of the SymbolType enumeratino (Type/Symbol/Enum), defaults to Type
+            offset: The location of the object, ignored when symbol_type is SYMBOL 
             native_layer_name: Name of the layer in which constructed objects are made (for pointers)
         """
-        type_arg = None  # type: Optional[Union[str, interfaces.objects.Template]]
-        if symbol_name is not None:
-            if constants.BANG in symbol_name:
-                raise ValueError("Symbol_name cannot reference another module")
-            symbol = self._context.symbol_space.get_symbol(self.symbol_table_name + constants.BANG + symbol_name)
-            if symbol.type is None:
-                raise ValueError("Symbol {} has no associated type information".format(symbol.name))
-            type_arg = symbol.type
-            offset = symbol.address
-            if not self._absolute_symbol_addresses:
-                offset += self._offset
-        elif type_name is not None and offset is not None:
-            if constants.BANG in type_name:
-                raise ValueError("Type_name cannot reference another module")
-            type_arg = self.symbol_table_name + constants.BANG + type_name
+        if constants.BANG not in symbol:
+            symbol = self.symbol_table_name + constants.BANG + symbol
         else:
-            raise ValueError("One of symbol_name, or type_name & offset, must be specified to construct a module")
+            raise ValueError("Cannot reference another module when constructing an object")
+
+        # Only set the offset if type is Symbol and we were given a name, not a template
+        if symbol_type == constants.SymbolType.SYMBOL:
+            offset = self._context.symbol_space.get_symbol(symbol).address
+
+        if offset is None:
+            raise ValueError("Offset must not be None for non-symbol objects")
+
+        if not absolute:
+            offset += self._offset
+
         # Ensure we don't use a layer_name other than the module's, why would anyone do that?
         if 'layer_name' in kwargs:
             del kwargs['layer_name']
-        return self._context.object(type_arg, self._layer_name, offset, native_layer_name, **kwargs)
+        return self._context.object(
+            symbol = symbol,
+            symbol_type = symbol_type,
+            layer_name = self._layer_name,
+            offset = offset,
+            native_layer_name = native_layer_name,
+            **kwargs)
 
     get_symbol = get_module_wrapper('get_symbol')
     get_type = get_module_wrapper('get_type')
@@ -216,16 +233,14 @@ class SizedModule(Module):
                  offset: int,
                  size: int,
                  symbol_table_name: Optional[str] = None,
-                 native_layer_name: Optional[str] = None,
-                 absolute_symbol_addresses: bool = False) -> None:
+                 native_layer_name: Optional[str] = None) -> None:
         super().__init__(
             context,
             module_name = module_name,
             layer_name = layer_name,
             offset = offset,
             native_layer_name = native_layer_name,
-            symbol_table_name = symbol_table_name,
-            absolute_symbol_addresses = absolute_symbol_addresses)
+            symbol_table_name = symbol_table_name)
         self._size = size
 
     @property

volatility/framework/interfaces/context.py
@@ -27,7 +27,7 @@ import copy
 from abc import ABCMeta, abstractmethod
 from typing import Optional, Union
 
-from volatility.framework import interfaces
+from volatility.framework import interfaces, constants
 
 
 class ContextInterface(object, metaclass = ABCMeta):
@@ -115,8 +115,7 @@ class ModuleInterface(metaclass = ABCMeta):
                  layer_name: str,
                  offset: int,
                  symbol_table_name: Optional[str] = None,
-                 native_layer_name: Optional[str] = None,
-                 absolute_symbol_addresses: bool = False) -> None:
+                 native_layer_name: Optional[str] = None) -> None:
         self._context = context
         self._module_name = module_name
         self._layer_name = layer_name
@@ -125,7 +124,6 @@ class ModuleInterface(metaclass = ABCMeta):
         if native_layer_name:
             self._native_layer_name = native_layer_name
         self.symbol_table_name = symbol_table_name or self._module_name
-        self._absolute_symbol_addresses = absolute_symbol_addresses
         super().__init__()
 
     @property
@@ -148,7 +146,12 @@ class ModuleInterface(metaclass = ABCMeta):
         return self._context
 
     @abstractmethod
-    def object(self, symbol_name: str = None, type_name: str = None, offset: int = None,
+    def object(self,
+               symbol: Union[str, 'interfaces.objects.Template'],
+               symbol_type: Optional[constants.SymbolType] = None,
+               offset: int = None,
+               native_layer_name: Optional[str] = None,
+               absolute: bool = False,
                **kwargs) -> 'interfaces.objects.ObjectInterface':
         """Returns an object created using the symbol_table_name and layer_name of the Module"""
 

volatility/framework/layers/msf.py
@@ -63,8 +63,7 @@ class PdbMultiStreamFormat(interfaces.layers.TranslationLayerInterface):
         root_table_num_pages = math.ceil(self._header.StreamInfo.StreamInfoSize / self._header.PageSize)
         root_index_size = math.ceil((root_table_num_pages * entry_size) / self._header.PageSize)
         root_index = module.object(
-            type_name = "array",
-            layer_name = self._base_layer,
+            symbol = "array",
             offset = self._header.vol.size,
             count = root_index_size,
             subtype = module.get_type("unsigned long"))
@@ -73,14 +72,14 @@ class PdbMultiStreamFormat(interfaces.layers.TranslationLayerInterface):
 
         module = self.context.module(self.pdb_symbol_table, root_index_layer_name, offset = 0)
         root_pages = module.object(
-            type_name = "array", offset = 0, count = root_table_num_pages, subtype = module.get_type("unsigned long"))
+            symbol = "array", offset = 0, count = root_table_num_pages, subtype = module.get_type("unsigned long"))
         root_layer_name = self.create_stream_from_pages("root", self._header.StreamInfo.StreamInfoSize,
                                                         [x for x in root_pages])
 
         module = self.context.module(self.pdb_symbol_table, root_layer_name, offset = 0)
-        num_streams = module.object(type_name = "unsigned long", offset = 0)
+        num_streams = module.object(symbol = "unsigned long", offset = 0)
         stream_sizes = module.object(
-            type_name = "array", offset = entry_size, count = num_streams, subtype = module.get_type("unsigned long"))
+            symbol = "array", offset = entry_size, count = num_streams, subtype = module.get_type("unsigned long"))
 
         current_offset = (num_streams + 1) * entry_size
 
@@ -90,7 +89,7 @@ class PdbMultiStreamFormat(interfaces.layers.TranslationLayerInterface):
                 self._streams[stream] = None
             else:
                 stream_page_list = module.object(
-                    type_name = "array",
+                    symbol = "array",
                     offset = current_offset,
                     count = list_size,
                     subtype = module.get_type("unsigned long"))

volatility/framework/plugins/linux/check_afinfo.py
@@ -98,7 +98,7 @@ class Check_afinfo(plugins.PluginInterface):
                 except exceptions.SymbolError:
                     continue
 
-                global_var = vmlinux.object(type_name = struct_type, offset = global_var.address)
+                global_var = vmlinux.object(symbol = struct_type, offset = global_var.address)
 
                 for name, member, address in self._check_afinfo(global_var_name, global_var, op_members, seq_members):
                     yield 0, (name, member, format_hints.Hex(address))

commit 3318e9c4a57e8c1a89e4325afa2cdd49c4ae667e

Fix a typo introduced in 5362e2094edbe2fce8b9256577a0753e175b9dcb.

volatility/framework/plugins/windows/handles.py
@@ -216,7 +216,8 @@ class Handles(interfaces_plugins.PluginInterface):
         if not self.context.layers[virtual].is_valid(offset):
             return
 
-        table = ntkrnlmp.object(object_type = "array", offset = offset, subtype = subtype, count = int(count))
+        table = ntkrnlmp.object(
+            object_type = "array", offset = offset, subtype = subtype, count = int(count), absolute = True)
 
         layer_object = self.context.layers[virtual]
         masked_offset = (offset & layer_object.maximum_address)