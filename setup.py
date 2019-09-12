3142ffdd698b341380e3271f694a28fc723ad211
Mike Auty <mike.auty@gmail.com>
2019-09-12 17:09:32 +0100

    Add in sphinx-rtd-theme to doc requirements.

--- setup.py
+++ setup.py
@@ -37,5 +37,5 @@ setuptools.setup(
         'jsonschema': ["jsonschema>=2.3.0"],
         'yara': ["yara-python>=3.8.0"],
         'disasm': ["capstone;platform_system=='Linux'", "capstone-windows;platform_system=='Windows'"],
-        'doc': ["sphinx>=1.8.2", "sphinx_autodoc_typehints>=1.4.0"],
+        'doc': ["sphinx>=1.8.2", "sphinx_autodoc_typehints>=1.4.0", "sphinx-rtd-theme>=0.4.3"],
     })
