337d2ee01657fa2886da0d69d47b2daadc08df4e
Mike Auty <mike.auty@gmail.com>
2019-10-27 16:08:39 +0000

    CLI: Move progress output to stderr from stdout.
    Fixes issue #104.

--- volatility/cli/__init__.py
+++ volatility/cli/__init__.py
@@ -57,7 +57,7 @@ class PrintedProgress(object):
         message = "\rProgress: {0: 7.2f}\t\t{1:}".format(round(progress, 2), description or '')
         message_len = len(message)
         self._max_message_len = max([self._max_message_len, message_len])
-        print(message, end = (' ' * (self._max_message_len - message_len)) + '\r')
+        sys.stderr.write(message + (' ' * (self._max_message_len - message_len)) + '\r')
 
 
 class MuteProgress(PrintedProgress):
