20dbe41eeea29bfc401c4bc253dbbb8b7fd690c0
Mike Auty <mike.auty@gmail.com>
2020-06-10 19:49:45 +0100

    Development: Improve scripts for operating over Mac KDK

--- development/mac-kdk/extract_kernel.sh
+++ development/mac-kdk/extract_kernel.sh
@@ -45,3 +45,6 @@ popd
 rm -fr tmp
 
 ${DWARF2JSON} mac --macho "${UNPACK_DIR}/${KERNEL_DIR}/kernel.dSYM" --macho-symbols "${UNPACK_DIRECTORY}/${KERNEL_DIR}/kernel" | xz -9 > ${JSON_DIR}/${KERNEL_DIR}.json.xz
+if [ $? == 0 ]; then
+  ${DWARF2JSON} mac --arch i386 --macho "${UNPACK_DIR}/${KERNEL_DIR}/kernel.dSYM" --macho-symbols "${UNPACK_DIRECTORY}/${KERNEL_DIR}/kernel" | xz -9 > ${JSON_DIR}/${KERNEL_DIR}.json.xz
+fi
