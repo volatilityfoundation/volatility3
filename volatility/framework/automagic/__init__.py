8b7de2f80ce2300a5f54130fe3ec625ce6cb8d84
ikelos <mike.auty@gmail.com>
2020-07-01 01:24:50 +0100

    Revert "Revert "LayerStacker: Support backwards compatbility""
    This reverts commit fbff5a550bac001e81a7bc79192c2b61492afee8.

--- volatility/framework/automagic/__init__.py
+++ volatility/framework/automagic/__init__.py
@@ -21,11 +21,15 @@ from volatility.framework.configuration import requirements
 
 vollog = logging.getLogger(__name__)
 
-windows_automagic = ['ConstructionMagic', 'LayerStacker', 'WintelHelper', 'KernelPDBScanner', 'WinSwapLayers']
+windows_automagic = [
+    'ConstructionMagic', 'LayerStacker', 'TranslationLayerStacker', 'WintelHelper', 'KernelPDBScanner', 'WinSwapLayers'
+]
 
-linux_automagic = ['ConstructionMagic', 'LayerStacker', 'LinuxBannerCache', 'LinuxSymbolFinder']
+linux_automagic = [
+    'ConstructionMagic', 'LayerStacker', 'TranslationLayerStacker', 'LinuxBannerCache', 'LinuxSymbolFinder'
+]
 
-mac_automagic = ['ConstructionMagic', 'LayerStacker', 'MacBannerCache', 'MacSymbolFinder']
+mac_automagic = ['ConstructionMagic', 'LayerStacker', 'TranslationLayerStacker', 'MacBannerCache', 'MacSymbolFinder']
 
 
 def available(context: interfaces.context.ContextInterface) -> List[interfaces.automagic.AutomagicInterface]:
