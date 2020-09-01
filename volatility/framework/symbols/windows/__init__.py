489dd0cf77bd5bea1d1d502084adf30655a3d250
memoryforensics1 <61626429+memoryforensics1@users.noreply.github.com>
2020-09-01 17:38:02 +0300

    add KTHREAD struct

--- volatility/framework/symbols/windows/__init__.py
+++ volatility/framework/symbols/windows/__init__.py
@@ -14,6 +14,7 @@ class WindowsKernelIntermedSymbols(intermed.IntermediateSymbolTable):
 
         # Set-up windows specific types
         self.set_type_class('_ETHREAD', extensions.ETHREAD)
+        self.set_type_class('_KTHREAD', extensions.KTHREAD)
         self.set_type_class('_LIST_ENTRY', extensions.LIST_ENTRY)
         self.set_type_class('_EPROCESS', extensions.EPROCESS)
         self.set_type_class('_UNICODE_STRING', extensions.UNICODE_STRING)
