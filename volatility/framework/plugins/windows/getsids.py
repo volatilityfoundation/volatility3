92c7d8852cf2ae8206fad6e40246721cca4ac344
memoryforensics1 <61626429+memoryforensics1@users.noreply.github.com>
2020-09-22 20:20:38 +0300

    fix

--- volatility/framework/plugins/windows/getsids.py
+++ volatility/framework/plugins/windows/getsids.py
@@ -4,14 +4,14 @@
 import logging
 import re, ntpath, os, json
 from typing import Callable, List, Generator, Iterable, Dict
-from volatility.framework import renderers, interfaces, objects, exceptions, constants
+from volatility.framework import renderers, interfaces, objects, exceptions, constants, layers
 from volatility.framework.configuration import requirements
 from volatility.framework.objects import utility
 from volatility.framework.renderers import format_hints
 from volatility.plugins.windows import pslist
-import volatility.framework.layers.registry
+from volatility.plugins.windows.registry import hivelist
 import volatility.framework.symbols.windows.extensions.registry as registry
-import volatility.plugins.windows.registry.hivelist as hivelist
+
 
 
 vollog = logging.getLogger(__name__)
@@ -89,7 +89,7 @@ class GetSIDs(interfaces.plugins.PluginInterface):
                     for node in subkey.get_values():
                         try:
                             value_node_name = node.get_name() or "(Default)"
-                        except (exceptions.InvalidAddressException, volatility.framework.layers.registry.RegistryFormatException) as excp:
+                        except (exceptions.InvalidAddressException, layers.registry.RegistryFormatException) as excp:
                             continue
                         try:
                             value_data = node.decode_data()
@@ -107,7 +107,7 @@ class GetSIDs(interfaces.plugins.PluginInterface):
                                 path = str(value_data).replace('\\x00', '')[:-1]
                                 user = ntpath.basename(path)
                                 sids[sid] = user
-                        except (ValueError, exceptions.InvalidAddressException, volatility.framework.layers.registry.RegistryFormatException) as excp:
+                        except (ValueError, exceptions.InvalidAddressException, layers.registry.RegistryFormatException) as excp:
                             continue
             except (KeyError, exceptions.InvalidAddressException):
                 continue
