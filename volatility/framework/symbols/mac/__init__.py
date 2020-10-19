b5d50c055b48e5d4cf0c9cc0e7490a2719811b3c
Andrew Case <acase@volexity.com>
2020-10-19 09:54:44 -0500

    Add the Mac kevents plugin

--- volatility/framework/symbols/mac/__init__.py
+++ volatility/framework/symbols/mac/__init__.py
@@ -36,8 +36,9 @@ class MacUtilities(interfaces.configuration.VersionableInterface):
     """
     Version History:
     1.1.0 -> added walk_list_head API
+    1.2.0 -> added walk_slist API
     """
-    _version = (1, 1, 0)
+    _version = (1, 2, 0)
 
     @classmethod
     def mask_mods_list(cls, context: interfaces.context.ContextInterface, layer_name: str,
@@ -203,4 +204,12 @@ class MacUtilities(interfaces.configuration.VersionableInterface):
         for element in cls._walk_iterable(queue, "lh_first", "le_next", next_member, max_elements):
             yield element
 
+    @classmethod
+    def walk_slist(cls,
+                   queue : interfaces.objects.ObjectInterface,
+                   next_member: str,
+                   max_elements: int = 4096) -> Iterable[interfaces.objects.ObjectInterface]:
+            
+        for element in cls._walk_iterable(queue, "slh_first", "sle_next", next_member, max_elements):
+            yield element    
 
