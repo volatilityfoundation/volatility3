c622ee4897c90421b98c61ae298a123dda199cd8
Mike Auty <mike.auty@gmail.com>
2020-10-30 14:59:01 +0000

    Plugins: Bump framework version for banners

--- volatility/framework/plugins/banners.py
+++ volatility/framework/plugins/banners.py
@@ -15,6 +15,8 @@ vollog = logging.getLogger(__name__)
 class Banners(interfaces.plugins.PluginInterface):
     """Attempts to identify potential linux banners in an image"""
 
+    _required_framework_version = (2, 0, 0)
+
     @classmethod
     def get_requirements(cls) -> List[interfaces.configuration.RequirementInterface]:
         return [requirements.TranslationLayerRequirement(name = 'primary', description = 'Memory layer to scan')]
