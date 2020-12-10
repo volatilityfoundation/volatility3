f9102a60b801c26899e966c6a65d34e18d47ba05
Mike Auty <mike.auty@gmail.com>
2020-12-10 22:42:16 +0000

    Release: Update the version number for this release
    Entering code-freeze
    10th Dec - Bug fixes and documentation, no new features
    7th Jan - Only critical fixes
    21st Jan - Release date

--- volatility/framework/constants/__init__.py
+++ volatility/framework/constants/__init__.py
@@ -41,7 +41,7 @@ BANG = "!"
 VERSION_MAJOR = 2  # Number of releases of the library with a breaking change
 VERSION_MINOR = 0  # Number of changes that only add to the interface
 VERSION_PATCH = 0  # Number of changes that do not change the interface
-VERSION_SUFFIX = "-beta.1"
+VERSION_SUFFIX = ""
 
 PACKAGE_VERSION = ".".join([str(x) for x in [VERSION_MAJOR, VERSION_MINOR, VERSION_PATCH]]) + VERSION_SUFFIX
 """The canonical version of the volatility package"""
