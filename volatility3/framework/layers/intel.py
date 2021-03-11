3e04b347cd08c1b31b2b2b8a48422dbac5e83031
Frank Block <f-block@users.noreply.github.com>
2021-03-11 23:54:03 +0100

    Added Comment/TODO for transition state issue
    See https://github.com/volatilityfoundation/volatility3/pull/475

--- volatility3/framework/layers/intel.py
+++ volatility3/framework/layers/intel.py
@@ -329,6 +329,10 @@ class WindowsIntelPAE(WindowsMixin, IntelPAE):
 
 class WindowsIntel32e(WindowsMixin, Intel32e):
 
+    # TODO: Fix appropriately in a future release.
+    # Currently just a temprorary workaround to deal with custom bit flag
+    # in the PFN field for pages in transition state.
+    # See https://github.com/volatilityfoundation/volatility3/pull/475
     _maxphyaddr = 45
     
     def _translate(self, offset: int) -> Tuple[int, int, str]:
