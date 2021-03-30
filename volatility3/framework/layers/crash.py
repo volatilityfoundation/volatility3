f5e5fd00609e20950df80801ae17f25b44103631
iMHLv2 <mligh@volexity.com>
2021-03-30 17:25:32 -0500

    apply fixes and improvements for crash layer (see description)
    1) Remove empty newlines before the license
    
    2) Remove unused imports
    
    3) Add support for 32-bit Bitmap crash dumps
    
    4) Move _SUMMARY_DUMP to crash_common.json and fix the swapped Pages and BitmapSize offsets
    
    5) Fix other errors in crash64.json (swapped SystemTime vs SystemUpTime, PsActiveProcessHead should be unsigned long long, several incorrect offsets for other members
    
    6) Switched to new volatility3 namespace
    
    7) Reverted required_framework_version to (1, 0, 0)
    
    8) Fixed crashinfo plugin from unpacking the wrong number of values from layer.mapping(). Actually, the plugin no longer displays runs - it shows metadata instead.
    
    9) Address Ikelos' comments in PR #452

--- volatility3/framework/layers/crash.py
+++ volatility3/framework/layers/crash.py
@@ -1,15 +1,13 @@
-
-  
 # This file is Copyright 2021 Volatility Foundation and licensed under the Volatility Software License 1.0
 # which is available at https://www.volatilityfoundation.org/license/vsl-v1.0
 #
 import logging
 import struct
-from typing import Tuple, Optional, Iterable
+from typing import Tuple, Optional
 
-from volatility.framework import constants, exceptions, interfaces
-from volatility.framework.layers import segmented
-from volatility.framework.symbols import intermed
+from volatility3.framework import constants, exceptions, interfaces
+from volatility3.framework.layers import segmented
+from volatility3.framework.symbols import intermed
 
 vollog = logging.getLogger(__name__)
 
@@ -30,7 +28,7 @@ class WindowsCrashDump32Layer(segmented.SegmentedLayer):
     VALIDDUMP = 0x504d5544
 
     crashdump_json = 'crash'
-    supported_dumptypes = [0x01]
+    supported_dumptypes = [0x01, 0x05] # we need 0x5 for 32-bit bitmaps
     dump_header_name = '_DUMP_HEADER'
 
     _magic_struct = struct.Struct('<II')
@@ -42,62 +40,118 @@ class WindowsCrashDump32Layer(segmented.SegmentedLayer):
         self._context = context
         self._config_path = config_path
         self._page_size = 0x1000
-        try: 
-            self._base_layer = self.config["base_layer"]
-        except KeyError:
-            self._base_layer = 'base_layer'
-            self.config['base_layer']='base_layer'
+        # no try/except needed. as seen in vmware.py
+        self._base_layer = self.config["base_layer"]
 
         # Create a custom SymbolSpace
         self._crash_table_name = intermed.IntermediateSymbolTable.create(context, self._config_path, 'windows',
                                                                          self.crashdump_json)
+
+        # the _SUMMARY_DUMP is shared between 32- and 64-bit
+        self._crash_common_table_name = intermed.IntermediateSymbolTable.create(context, self._config_path,
+                                                                                'windows',
+                                                                                'crash_common')
+
         # Check Header
         hdr_layer = self._context.layers[self._base_layer]
         hdr_offset = 0
         self.check_header(hdr_layer, hdr_offset)
 
         # Need to create a header object
-        header = self.context.object(self._crash_table_name + constants.BANG + self.dump_header_name,
-                                     offset = hdr_offset,
-                                     layer_name = self._base_layer)
+        self._header = self.context.object(self._crash_table_name + constants.BANG + self.dump_header_name,
+                                           offset = hdr_offset,
+                                           layer_name = self._base_layer)
 
         # Extract the DTB
-        self.dtb = int(header.DirectoryTableBase)
+        self.dtb = int(self._header.DirectoryTableBase)
 
-        self.dump_type = int(header.DumpType)
+        self.dump_type = int(self._header.DumpType)
 
         # Verify that it is a supported format
-        if header.DumpType not in self.supported_dumptypes:
-            vollog.log(constants.LOGLEVEL_VVVV, "unsupported dump format 0x{:x}".format(header.DumpType))
-            raise WindowsCrashDumpFormatException(name, "unsupported dump format 0x{:x}".format(header.DumpType))
+        if self._header.DumpType not in self.supported_dumptypes:
+            vollog.log(constants.LOGLEVEL_VVVV, "unsupported dump format 0x{:x}".format(self._header.DumpType))
+            raise WindowsCrashDumpFormatException(name, "unsupported dump format 0x{:x}".format(self._header.DumpType))
 
+        # Then call the super, which will call load_segments (which needs the base_layer before it'll work)
         super().__init__(context, config_path, name)
+
+    def get_header(self) -> interfaces.objects.ObjectInterface:
+        return self._header
 
     def _load_segments(self) -> None:
         """Loads up the segments from the meta_layer."""
-        
 
         segments = []
 
-        offset = self.headerpages
-        header = self.context.object(self._crash_table_name + constants.BANG + self.dump_header_name,
-                                 offset = 0,
-                                 layer_name = self._base_layer)
-        offset = self.headerpages                      
-        header.PhysicalMemoryBlockBuffer.Run.count = header.PhysicalMemoryBlockBuffer.NumberOfRuns
-        for x in header.PhysicalMemoryBlockBuffer.Run:
-            segments.append((x.BasePage * 0x1000, offset * 0x1000, x.PageCount * 0x1000, x.PageCount * 0x1000))
-                # print("Segments {:x} {:x} {:x}".format(x.BasePage * 0x1000,
-                #                  offset * 0x1000,
-                #                  x.PageCount * 0x1000))
-            offset += x.PageCount
+        # instead of hard coding 0x2000, use 0x1000 * self.headerpages so this works for
+        # both 32- and 64-bit dumps
+        summary_header = self.context.object(self._crash_common_table_name + constants.BANG + "_SUMMARY_DUMP",
+                                             offset=0x1000 * self.headerpages,
+                                             layer_name=self._base_layer)
+        if self.dump_type == 0x1:
+            header = self.context.object(self._crash_table_name + constants.BANG + self.dump_header_name,
+                                         offset=0,
+                                         layer_name=self._base_layer)
 
+            offset = self.headerpages
+            header.PhysicalMemoryBlockBuffer.Run.count = header.PhysicalMemoryBlockBuffer.NumberOfRuns
+            for x in header.PhysicalMemoryBlockBuffer.Run:
+                segments.append((x.BasePage * 0x1000, offset * 0x1000, x.PageCount * 0x1000, x.PageCount * 0x1000))
+                offset += x.PageCount
+
+        elif self.dump_type == 0x05:
+
+            ## NOTE: In the original crash64.json, _SUMMARY_DUMP.Pages was offset 48 and
+            ## _SUMMARY_DUMP.BitmapSize was offset 40. From the volatility2 vtypes, that is
+            ## backwards! The correct offsets should be:
+            ##
+            ## 'Pages' : [ 0x28, ['unsigned long long']],    -> This is 40 decimal
+            ## 'BitmapSize': [0x30, ['unsigned long long']], -> This is 48 decimal
+            ##
+            ## Most likely, some of the code that follows needs to be adjusted with those
+            ## newly refactored offsets in mind. I commented out the "+ 0x2000" below, and
+            ## this still works on Win10x64_17763_crash.dmp, but it fails on
+            ## the Win10x86_17763_crash.dmp version. 
+
+            # Add 0x2000 as some bitmaps are too short by one offset
+            summary_header.BufferLong.count = (summary_header.BitmapSize + 31) // 32 # + 0x2000
+            previous_bit = 0
+            start_position = 0
+            # We cast as an int because we don't want to carry the context around with us for infinite loop reasons
+            mapped_offset = int(summary_header.HeaderSize)
+            current_word = None
+            bitmap_len = len(summary_header.BufferLong) * 32
+
+            for bit_position in range(bitmap_len):
+                if (bit_position % 32) == 0:
+                    current_word = summary_header.BufferLong[bit_position // 32]
+                current_bit = (current_word >> (bit_position % 32)) & 1
+
+                if current_bit != previous_bit:
+                    if previous_bit == 0:
+                        # Start
+                        start_position = bit_position
+                    else:
+                        # Finish
+                        length = (bit_position - start_position) * 0x1000
+                        segments.append((start_position * 0x1000, mapped_offset, length, length))
+                        mapped_offset += length
+
+                # Find the last segment in a file which will be at the end or two pages from the end. We multiply by 32 as we want to offset bby words rather than bits
+                if (bit_position == bitmap_len - 1 or bit_position == bitmap_len - 1 - 32 * 0x2000) and current_bit == 1:
+                    length = (bit_position - start_position) * 0x1000
+                    segments.append((start_position * 0x1000, mapped_offset, length, length))
+                    mapped_offset += length
+                    break
+                previous_bit = current_bit
+        else:
+            vollog.log(constants.LOGLEVEL_VVVV, "unsupported dump format 0x{:x}".format(self.dump_type))
+            raise WindowsCrashDumpFormatException(self.name, "unsupported dump format 0x{:x}".format(self.dump_type))
 
         if len(segments) == 0:
             raise WindowsCrashDumpFormatException(self.name, "No Crash segments defined in {}".format(self._base_layer))
+
         self._segments = segments
-
-
 
     @classmethod
     def check_header(cls, base_layer: interfaces.layers.DataLayerInterface, offset: int = 0) -> Tuple[int, int]:
@@ -131,69 +185,6 @@ class WindowsCrashDump64Layer(WindowsCrashDump32Layer):
     dump_header_name = '_DUMP_HEADER64'
     supported_dumptypes = [0x1, 0x05]
     headerpages = 2
-
-    def _load_segments(self) -> None:
-        """Loads up the segments from the meta_layer."""
-
-        segments = []
-
-        summary_header = self.context.object(self._crash_table_name + constants.BANG + "_SUMMARY_DUMP64",
-                                             offset = 0x2000,
-                                             layer_name = self._base_layer)
-        if self.dump_type == 0x1:
-            header = self.context.object(self._crash_table_name + constants.BANG + self.dump_header_name,
-                                         offset = 0,
-                                         layer_name = self._base_layer)
-
-            offset = self.headerpages
-            header.PhysicalMemoryBlockBuffer.Run.count = header.PhysicalMemoryBlockBuffer.NumberOfRuns
-            for x in header.PhysicalMemoryBlockBuffer.Run:
-                segments.append((x.BasePage * 0x1000, offset * 0x1000, x.PageCount * 0x1000, x.PageCount * 0x1000))
-                offset += x.PageCount
-
-        elif self.dump_type == 0x05:
-            #Add 0x2000 as some bitmaps are too short by one offset
-            summary_header.BufferLong.count = (summary_header.BitmapSize + 31) // 32 + 0x2000 
-            previous_bit = 0
-            start_position = 0
-            # We cast as an int because we don't want to carry the context around with us for infinite loop reasons
-            mapped_offset = int(summary_header.HeaderSize)
-            current_word = None
-            bitmap_len=len(summary_header.BufferLong) * 32
-            for bit_position in range(bitmap_len):
-                if (bit_position % 32) == 0:
-                    current_word = summary_header.BufferLong[bit_position // 32]
-                current_bit = (current_word >> (bit_position % 32)) & 1
-
-                if current_bit != previous_bit:
-                    if previous_bit == 0:
-                        # Start
-                        start_position = bit_position
-                    else:
-                        # Finish
-                        length = (bit_position - start_position) * 0x1000
-                        segments.append((start_position * 0x1000, mapped_offset, length, length))
-                        mapped_offset += length
-
-
-                # Find the last segment in a file which will be at the end or two pages from the end. We multiply by 32 as we want to offset bby words rather than bits
-                if (bit_position == bitmap_len - 1 or bit_position == bitmap_len - 1 -32*0x2000) and current_bit == 1:
-                    length = (bit_position - start_position) * 0x1000
-                    segments.append((start_position * 0x1000, mapped_offset, length, length))
-                    mapped_offset += length
-                    break
-
-
-
-                previous_bit = current_bit
-        else:
-            vollog.log(constants.LOGLEVEL_VVVV, "unsupported dump format 0x{:x}".format(self.dump_type))
-            raise WindowsCrashDumpFormatException(self.name, "unsupported dump format 0x{:x}".format(self.dump_type))
-
-        if len(segments) == 0:
-            raise WindowsCrashDumpFormatException(self.name, "No Crash segments defined in {}".format(self._base_layer))
-        
-        self._segments = segments
 
 
 class WindowsCrashDumpStacker(interfaces.automagic.StackerLayerInterface):
--- volatility3/framework/plugins/windows/crashinfo.py
+++ volatility3/framework/plugins/windows/crashinfo.py
@@ -1,18 +1,18 @@
-  
 # This file is Copyright 2021 Volatility Foundation and licensed under the Volatility Software License 1.0
 # which is available at https://www.volatilityfoundation.org/license/vsl-v1.0
 #
 
 import logging
-from volatility.framework import interfaces, renderers
-from volatility.framework.configuration import requirements
-from volatility.framework.layers import crash
-from volatility.framework import exceptions
+import datetime
+from volatility3.framework import interfaces, renderers
+from volatility3.framework.configuration import requirements
+from volatility3.framework.renderers import format_hints, conversion
+from volatility3.framework.objects import utility
 
 vollog = logging.getLogger(__name__)
 
 class Crashinfo(interfaces.plugins.PluginInterface):
-    _required_framework_version = (2, 0, 0)
+    _required_framework_version = (1, 0, 0)
 
     @classmethod
     def get_requirements(cls):
@@ -23,11 +23,39 @@ class Crashinfo(interfaces.plugins.PluginInterface):
             ]
         
     def _generator(self, layer):
-        for offset, length, mapped_offset in layer.mapping(0x0, layer.maximum_address, ignore_errors = True):
-            yield(0,(offset,length,mapped_offset))
+        header = layer.get_header()
+        uptime = datetime.timedelta(microseconds=int(header.SystemUpTime) / 10)
+
+        yield(0, (utility.array_to_string(header.Signature),
+                  header.MajorVersion,
+                  header.MinorVersion,
+                  format_hints.Hex(header.DirectoryTableBase),
+                  format_hints.Hex(header.PfnDataBase),
+                  format_hints.Hex(header.PsLoadedModuleList),
+                  format_hints.Hex(header.PsActiveProcessHead),
+                  header.MachineImageType,
+                  header.NumberProcessors,
+                  format_hints.Hex(header.KdDebuggerDataBlock),
+                  header.DumpType,
+                  str(uptime),
+                  utility.array_to_string(header.Comment),
+                  conversion.wintime_to_datetime(header.SystemTime),
+                  ))
 
     def run(self):
-
         layer = self._context.layers[self.config['primary.memory_layer']]
-
-        return renderers.TreeGrid([("StartAddress", int),("FileOffset", int),("Length", int)],self._generator(layer))
+        return renderers.TreeGrid([("Signature", str),
+                                   ("MajorVersion", int),
+                                   ("MinorVersion", int),
+                                   ("DirectoryTableBase", format_hints.Hex),
+                                   ("PfnDataBase", format_hints.Hex),
+                                   ("PsLoadedModuleList", format_hints.Hex),
+                                   ("PsActiveProcessHead", format_hints.Hex),
+                                   ("MachineImageType", int),
+                                   ("NumberProcessors", int),
+                                   ("KdDebuggerDataBlock", format_hints.Hex),
+                                   ("DumpType", int),
+                                   ("SystemUpTime", str),
+                                   ("Comment", str),
+                                   ("SystemTime", datetime.datetime),
+                                   ], self._generator(layer))
