57e1243932168b7c3c179ac4aba7507d8af483dd
Mike Auty <mike.auty@gmail.com>
2021-08-27 22:23:33 +0100

    CLI: Verify improvement for buildbot

--- vol.py
+++ vol.py
@@ -4,7 +4,11 @@
 # which is available at https://www.volatilityfoundation.org/license/vsl-v1.0
 #
 
+import sys
 import volatility3.cli
+
+sys.stdin.reconfigure(encoding='utf-8')
+sys.stdout.reconfigure(encoding='utf-8')
 
 if __name__ == '__main__':
     volatility3.cli.main()
