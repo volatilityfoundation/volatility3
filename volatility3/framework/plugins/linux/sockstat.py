7ed5739e24d5f5c836a1cad2d2b0ddf73000f9a7
Gustavo Moreira <gmoreira@gmail.com>
2021-12-14 16:24:57 +1100

    socket is no longer  imported in this file. Remove the underscore

--- volatility3/framework/plugins/linux/sockstat.py
+++ volatility3/framework/plugins/linux/sockstat.py
@@ -310,13 +310,13 @@ class Sockstat(plugins.PluginInterface):
                 continue
 
             socket_alloc = linux.LinuxUtilities.container_of(d_inode, "socket_alloc", "vfs_inode", vmlinux)
-            _socket = socket_alloc.socket
+            socket = socket_alloc.socket
 
             vfs_inode = socket_alloc.vfs_inode
-            if not (_socket and vfs_inode):
+            if not (socket and vfs_inode):
                 continue
 
-            sock = _socket.sk.dereference()
+            sock = socket.sk.dereference()
 
             sock_type = sock.type
             family = sock.family
