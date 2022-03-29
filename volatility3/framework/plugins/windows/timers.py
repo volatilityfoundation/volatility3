d54dda3fbc71f9ca56d7d747a5fb14c2441955d8
Donghyun Kim <digitalisx99@gmail.com>
2022-03-30 01:13:02 +0900

    Plugins: Add PluginRequirement and Fix Treegrid column

--- volatility3/framework/plugins/windows/timers.py
+++ volatility3/framework/plugins/windows/timers.py
@@ -6,9 +6,10 @@ import logging
 
 from typing import Iterator, List, Tuple
 
-from volatility3.framework import renderers, interfaces
+from volatility3.framework import constants, renderers, interfaces
 from volatility3.framework.configuration import requirements
 from volatility3.framework.renderers import format_hints
+from volatility3.plugins.windows import pslist, modules
 
 vollog = logging.getLogger(__name__)
 
@@ -22,18 +23,22 @@ class Timers(interfaces.plugins.PluginInterface):
     def get_requirements(cls)-> List[interfaces.configuration.RequirementInterface]:
         return [
             requirements.ModuleRequirement(name = 'kernel', description = 'Windows kernel',
-                                           architectures = ["Intel32", "Intel64"])         
+                                           architectures = ["Intel32", "Intel64"]),
+            requirements.PluginRequirement(name = 'pslist', plugin = pslist.PsList, version = (2, 0, 0)),
+            requirements.PluginRequirement(name = 'modules', plugin = modules.Modules, version = (1, 1, 0)),  
         ]
-    
+
     def _generator(self) -> Iterator[Tuple]:
-        yield(0, [])
+        # TODO KiProcessorBlock → _KPCR
+        # TODO task.get_kdbg
+        kernel = self.context.modules[self.config['kernel']]
     
     def run(self)-> renderers.TreeGrid:
         return renderers.TreeGrid([
             ("Offset", format_hints.Hex),
             ("DueTime", str),
-            ("Period(ms)", str),
+            ("Period(ms)", int),
             ("Signaled", str),
-            ("Routine", int),
-            ("Module", format_hints.Hex)
+            ("Routine", format_hints.Hex),
+            ("Module", str)
         ], self._generator())
