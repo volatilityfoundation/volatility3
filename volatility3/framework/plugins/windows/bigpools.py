fd81dba42064414f93e20c724580502be2412528
iMHLv2 <mligh@volexity.com>
2022-04-20 13:21:43 -0500

    refs #668 bump the minor version and not the revision for an additive change

--- volatility3/framework/plugins/windows/bigpools.py
+++ volatility3/framework/plugins/windows/bigpools.py
@@ -21,7 +21,7 @@ class BigPools(interfaces.plugins.PluginInterface):
     """List big page pools."""
 
     _required_framework_version = (2, 0, 0)
-    _version = (1, 0, 1)
+    _version = (1, 1, 0)
 
     @classmethod
     def get_requirements(cls) -> List[interfaces.configuration.RequirementInterface]:
