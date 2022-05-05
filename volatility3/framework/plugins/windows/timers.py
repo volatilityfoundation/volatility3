f572487b38675a9c13b70ab9514725e73a442df3
Donghyun Kim <digitalisx99@gmail.com>
2022-05-06 00:22:07 +0900

    Refactor: get KPRCB object

--- volatility3/framework/plugins/windows/timers.py
+++ volatility3/framework/plugins/windows/timers.py
@@ -6,7 +6,7 @@ import logging
 
 from typing import Iterator, List, Tuple
 
-from volatility3.framework import constants, renderers, symbols, interfaces
+from volatility3.framework import layers, renderers, interfaces
 from volatility3.framework.configuration import requirements
 
 vollog = logging.getLogger(__name__)
@@ -23,37 +23,41 @@ class Timers(interfaces.plugins.PluginInterface):
             requirements.ModuleRequirement(name = 'kernel', description = 'Windows kernel',
                                            architectures = ["Intel32", "Intel64"])
         ]
+    
+    @classmethod
+    def get_kernel_module(cls, context: interfaces.context.ContextInterface, layer_name: str, symbol_table: str):
+        """Returns the kernel module based on the layer and symbol_table"""
+        virtual_layer = context.layers[layer_name]
+        if not isinstance(virtual_layer, layers.intel.Intel):
+            raise TypeError("Virtual Layer is not an intel layer")
+
+        kvo = virtual_layer.config["kernel_virtual_offset"]
+
+        ntkrnlmp = context.module(symbol_table, layer_name = layer_name, offset = kvo)
+        return ntkrnlmp
+    
+    @classmethod
+    def get_kpcr(cls, context: interfaces.context.ContextInterface, layer_name: str, symbol_table: str) -> \
+        interfaces.objects.ObjectInterface:
+        """Returns the KPCR structure for a kernel"""
+        
+        ntkrnlmp = cls.get_kernel_module(context, layer_name, symbol_table)
+        
+        kprcb_offset = ntkrnlmp.get_symbol("KiProcessorBlock").address
+        kprcb_object = ntkrnlmp.object("_KPRCB", offset = kprcb_offset, layer_name = layer_name)
+        return kprcb_object
 
     def _generator(self) -> Iterator[Tuple]:
+
         kernel = self.context.modules[self.config['kernel']]
         layer_name = kernel.layer_name
+        symbol_table = kernel.symbol_table_name
 
-        kvo = self.context.layers[layer_name].config['kernel_virtual_offset']
-        kernel_module = self.context.module(kernel.symbol_table_name, layer_name = layer_name, offset = kvo)
-        
-        kpb_offset = kernel_module.get_symbol("KiProcessorBlock").address
-        
-        # x86
-        if not symbols.symbol_table_is_64bit(self.context, kernel.symbol_table_name):
-            yield(0, [0, "TEST", 0, "TEST", 0, "TEST"])
-        # x64
-        else:
-            kpcr_size = kernel_module.get_type("_KPCR").size
-            pointer_size = self.context.modules[self.config['kernel']].get_type("pointer").size
-            cal_offset = kpb_offset - (kpcr_size + pointer_size)
-            print(kpb_offset, kpcr_size, cal_offset)
+        kpcr = self.get_kpcr(self.context, layer_name, symbol_table)
+        yield(0, ("KPCR Major/Minor", kpcr.CpuID.cast("string")))
 
-            kpcr = kernel_module.object(object_type = "_KPCR", offset = cal_offset)
-            print(kpcr.MajorVersion)
-            print(kpcr.MinorVersion)
-            yield(0, [0, "TEST", 0, "TEST", 0, "TEST"])
     
     def run(self)-> renderers.TreeGrid:
         return renderers.TreeGrid([
-            ("Offset", int),
-            ("DueTime", str),
-            ("Period(ms)", int),
-            ("Signaled", str),
-            ("Routine", int),
-            ("Module", str)
+            ("Variable", str), ("Value", str)
         ], self._generator())
