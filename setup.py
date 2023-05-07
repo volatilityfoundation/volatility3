9ed3b96d8afb2c98f72f3846d298acf4a5c9214b
Alexandre ZANNI <16578570+noraj@users.noreply.github.com>
2023-05-07 17:40:30 +0200

    fix deployment of tests
    fix #950

--- setup.py
+++ setup.py
@@ -40,7 +40,7 @@ setuptools.setup(
     include_package_data=True,
     exclude_package_data={"": ["development", "development.*"], "development": ["*"]},
     packages=setuptools.find_namespace_packages(
-        exclude=["development", "development.*"]
+        exclude=["development", "development.*", "tests*"]
     ),
     entry_points={
         "console_scripts": [
