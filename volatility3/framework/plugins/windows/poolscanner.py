7469872c8bfcd8d7a84637ede54180008e624b0a
RuBublik <relikr@gmail.com>
2023-05-17 21:41:37 +0300

    added 'PoolConstraint' of Thread objects to 'PoolScanner.default_constraints' as part of adding support for thread pool tag scanning

--- volatility3/framework/plugins/windows/poolscanner.py
+++ volatility3/framework/plugins/windows/poolscanner.py
@@ -224,6 +224,20 @@ class PoolScanner(plugins.PluginInterface):
                 size=(600, None),
                 page_type=PoolType.PAGED | PoolType.NONPAGED | PoolType.FREE,
             ),
+            # threads on windows before windows8
+            PoolConstraint(b'Thr\xe5',  # -> “protected” allocation, MSB is set.
+                type_name = symbol_table + constants.BANG + "_ETHREAD",
+                object_type="Thread",
+                size = (600, None),     # -> 0x0258 - size of strcut in win5.1
+                page_type = PoolType.PAGED | PoolType.NONPAGED | PoolType.FREE
+            ),
+            # threads on windows starting with windows8
+            PoolConstraint(b'Thre',
+                type_name = symbol_table + constants.BANG + "_ETHREAD",
+                object_type="Thread",
+                size = (600, None),     # -> 0x0258 - size of strcut in win5.1
+                page_type = PoolType.PAGED | PoolType.NONPAGED | PoolType.FREE
+            ),
             # files on windows before windows 8
             PoolConstraint(
                 b"Fil\xe5",
