9bdd249aadb5b60ca5e45c91c396f1c591f9b45e
RuBublik <relikr@gmail.com>
2023-05-22 15:39:12 +0300

    added _version to ThrdScan

--- volatility3/framework/plugins/windows/thrdscan.py
+++ volatility3/framework/plugins/windows/thrdscan.py
@@ -19,6 +19,7 @@ class ThrdScan(interfaces.plugins.PluginInterface, timeliner.TimeLinerInterface)
 
     # cuz installed Framework interface version 2
     _required_framework_version = (2, 0, 0)  
+    _version = (1, 0, 0)
 
     @classmethod
     def get_requirements(cls):
