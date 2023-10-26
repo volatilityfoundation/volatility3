b0cbd3c61cae8fce0a295021418ce1f798fb3ab9
k1nd0ne <k1nd0ne@mail.com>
2023-10-26 02:49:07 +0200

    Adding hibernation support

--- volatility3/framework/layers/codecs/__init__.py
+++ volatility3/framework/layers/codecs/__init__.py
@@ -3,6 +3,165 @@
 #
 
 """Codecs used for encoding or decoding data should live here
-
-
 """
+import io, struct
+
+def encoded_bit_length(data, symbol):
+    if (symbol % 2) == 0:
+        return int(data[symbol//2] & 0x0f)
+    else:
+        return int(data[symbol//2] >> 4)
+
+def Read16Bits(input, current_position):
+    return (input[current_position] << 8) | input[current_position + 1]
+
+def ReadByte(input, current_position):
+    stream = io.BytesIO(input)
+    stream.seek(current_position)
+    return int.from_bytes(stream.read(1),"little")
+
+def lz77_huffman_decompress(compressed_input):
+    decoding_table = [0]*pow(2,15)
+    current_table_entry = 0
+
+    for bit_length in range(1, 16):
+        for symbol in range(0, 512):
+            if encoded_bit_length(compressed_input[0:256],symbol) == bit_length:
+                entry_count = 1 << (15 - bit_length)
+                for _ in range(entry_count):
+                    if current_table_entry >= pow(2,15):
+                        raise ValueError("The compressed data is not valid.")
+                    decoding_table[current_table_entry] = symbol
+                    current_table_entry += 1
+        if current_table_entry != pow(2,15):
+            raise ValueError("The compressed data is not valid.")
+
+    output_buffer = []
+    current_position = 256
+    next_bits = Read16Bits(compressed_input,current_position)
+    current_position += 2
+    next_bits <<= 16
+    next_bits |= Read16Bits(compressed_input,current_position)
+    current_position += 2
+    extra_bit_count = 16
+    block_end = len(output_buffer) + 65536
+
+    while True:
+        # Start of new block
+        if current_position + 512 > len(compressed_input):
+            print("EOF reached. Terminating decoding.")
+            return output_buffer
+        if len(output_buffer) >= block_end:
+            break
+        next_15bits = next_bits >> (32 - 15)
+        huffman_symbol = decoding_table[next_15bits]
+        huffman_symbol_bit_length = encoded_bit_length(compressed_input[0:256],huffman_symbol)
+
+        next_bits <<= huffman_symbol_bit_length
+        extra_bit_count -= huffman_symbol_bit_length
+
+        if extra_bit_count < 0:
+            next_bits |= Read16Bits(compressed_input,current_position) << (-extra_bit_count)
+            extra_bit_count += 16
+            current_position += 2
+
+        if huffman_symbol < 256:
+            output_buffer.append(huffman_symbol)
+
+        elif huffman_symbol == 256 and current_position == len(compressed_input):
+            return output_buffer
+
+        else:
+            huffman_symbol -= 256
+            match_length = huffman_symbol % 16
+            match_offset_bit_length = huffman_symbol // 16
+            if match_length == 15:
+                match_length = ReadByte(compressed_input,current_position)
+                current_position += 1
+                if match_length == 255:
+                    match_length = Read16Bits(compressed_input,current_position)
+                    current_position += 2
+                    if match_length < 15:
+                        raise ValueError("The compressed data is invalid.")
+                    match_length -= 15
+                match_length += 15
+            match_length += 3
+            match_offset = next_bits >> (32 - match_offset_bit_length)
+            match_offset += pow(2, match_offset_bit_length)
+            next_bits <<= match_offset_bit_length
+            extra_bit_count -= match_offset_bit_length
+            if extra_bit_count < 0:
+                next_bits |= Read16Bits(compressed_input,current_position) << (-extra_bit_count)
+                extra_bit_count += 16
+                current_position += 2
+            for i in range(match_length):
+                output_buffer.append(output_buffer[len(output_buffer) - match_offset + i])
+
+def lz77_plain_decompress(in_buf):
+    out_idx = 0
+    in_idx = 0
+    nibble_idx = 0
+
+    flags = 0
+    flag_count = 0
+
+    out_buf = []
+
+    while in_idx < len(in_buf):
+        if flag_count == 0:
+            flags = struct.unpack('<I', in_buf[in_idx:in_idx+4])[0]
+            in_idx += 4
+            flag_count = 32
+
+        flag_count -= 1
+
+        if (flags & (1 << flag_count)) == 0:
+            out_buf.append(in_buf[in_idx])
+            in_idx += 1
+            out_idx += 1
+        else:
+            if in_idx == len(in_buf):
+                return bytes(out_buf)
+            
+            length = struct.unpack('<H', in_buf[in_idx:in_idx+2])[0]
+            in_idx += 2
+
+            offset = (length // 8) + 1
+            length = length % 8
+
+            if length == 7:
+                if nibble_idx == 0:
+                    length = in_buf[in_idx] % 16
+                    nibble_idx = in_idx
+                    in_idx += 1
+                else:
+                    length = in_buf[nibble_idx] // 16
+                    nibble_idx = 0
+
+                if length == 15:
+                    length = in_buf[in_idx]
+                    in_idx += 1
+
+                    if length == 255:
+                        length = struct.unpack('<H', in_buf[in_idx:in_idx+2])[0]
+                        in_idx += 2
+
+                        if length == 0:
+                            length = struct.unpack('<I', in_buf[in_idx:in_idx+4])[0]
+                            in_idx += 4
+
+                        if length < 15 + 7:
+                            raise ValueError('CorruptedData')
+                        length -= 15 + 7
+                    length += 15
+                length += 7
+            length += 3
+
+            for _ in range(length):
+                if offset > out_idx:
+                    raise ValueError('CorruptedData')
+                out_buf.append(out_buf[out_idx - offset])
+                out_idx += 1
+
+    return bytes(out_buf)
+
---
+++ volatility3/framework/layers/hib.py
@@ -0,0 +1,176 @@
+from typing import Optional, Tuple
+import logging, struct
+from volatility3.framework import automagic, interfaces, constants, exceptions
+from volatility3.framework.interfaces import context
+from volatility3.framework.automagic import stacker
+from volatility3.framework.layers import intel, physical, segmented
+from volatility3.framework.renderers import conversion
+from volatility3.framework.layers.codecs import lz77_plain_decompress, lz77_huffman_decompress
+
+
+vollog = logging.getLogger(__name__)
+
+
+def uncompress(data: bytes, huffman: bool):
+    if huffman ==1:
+        return lz77_huffman_decompress(data)
+    else:
+        return lz77_plain_decompress(data)
+
+
+def readBytes(data, position, num):
+    end = position + num
+    byte_vals = data[position:end]
+    return int.from_bytes(byte_vals, byteorder='big')
+
+class HibernationFileException(exceptions.LayerException):
+    """Thrown when an error occurs with the underlying Hibernation file format."""
+
+class HibernationLayer(segmented.NonLinearlySegmentedLayer):
+    """
+    A TranslationLayer that maps physical memory against a Microsoft Windows hibernation file (x64 only for now).
+    """
+    PAGE_SIZE = 4096
+    HEADER_SIZE = 4
+    PAGE_DESC_SIZE = 8
+    def __init__(self, context: interfaces.context.ContextInterface, config_path: str, name: str, **kwargs):
+        """
+        Initializes the Hibernation file layer.
+        """
+        # Call the superclass constructor.
+        self._compressed = {}
+        self._mapping = {}
+        super().__init__(context, config_path, name, **kwargs)
+
+    @classmethod
+    def _check_header(
+        cls, base_layer: interfaces.layers.DataLayerInterface, name: str = ""
+    ):
+        header = base_layer.read(0, 4)
+        if header != b'HIBR':
+            raise exceptions.LayerException(name, "No Hibernation magic bytes")
+        else: 
+            vollog.info("Detecting an hibernation file")
+
+    def _load_segments(self):
+        base_layer = self.context.layers[self._base_layer]
+        system_time = int.from_bytes(base_layer.read(0x020, 8), "little")
+        systemTime = conversion.wintime_to_datetime(system_time)
+        NumPagesForLoader = int.from_bytes(base_layer.read(0x058, 8), "little")
+        FirstBootRestorePage = int.from_bytes(base_layer.read(0x068, 8), "little") 
+        FirstKernelRestorePage = int.from_bytes(base_layer.read(0x070, 8), "little")
+        KernelPagesProcessed = int.from_bytes(base_layer.read(0x230, 8), "little")
+
+        # vollog.info(f"""
+        #         SystemTime : {systemTime} \n
+        #         NumPagesForLoader : {NumPagesForLoader} \n
+        #         FirstBootRestorePage : {hex(FirstBootRestorePage)} \n
+        #         KernelPageProcessed : {KernelPagesProcessed} \n
+        #         FirstKernelRestorePage : {FirstKernelRestorePage} \n       
+        # """)
+        # TODO : If the FirstKernelRestorePage member of the header is non-zero, 
+        # its value gives the page number of the start of the KernelRestore- Pages.
+        # We need to check if this value is zero and not process the KernelRestorePages if so.
+
+        offset = FirstBootRestorePage * self.PAGE_SIZE
+        total_pages = NumPagesForLoader
+        treated = 0
+
+        while total_pages > treated:
+            page_read, next_cs = self._read_compression_set(offset)
+            offset += next_cs
+            treated += page_read
+
+        offset = FirstKernelRestorePage * self.PAGE_SIZE
+        total_pages = KernelPagesProcessed
+       
+        treated = 0
+        while total_pages > treated:
+            page_read, next_cs = self._read_compression_set(offset)
+            offset += next_cs
+            treated += page_read        
+        
+        self._segments = sorted(self._segments, key=lambda x: x[0])
+
+    def _read_compression_set(self, offset):
+        """
+            Desc : Read one compression set an extract the address of the compressed data
+            Params : 
+                - offset : the location of the compression set to read.
+                - stream : the hibernation file stream.
+            Return : The offset of the compressed data and the size.
+        """
+        base_layer = self.context.layers[self._base_layer]
+
+        header = base_layer.read(offset, self.HEADER_SIZE)
+        data = struct.unpack('<I', header)[0]
+        number_of_descs = data & 0xff
+        size_of_compressed_data = (data >> 8) & 0x3fffff
+        huffman_compressed = (data >> 30) & 0x1
+        mapped_address = offset+self.HEADER_SIZE+number_of_descs*self.PAGE_DESC_SIZE
+        total_page_count = 0
+        position = 0
+        for i in range(number_of_descs):
+            location = offset+self.HEADER_SIZE+i*self.PAGE_DESC_SIZE
+            page_descriptor = base_layer.read(location, self.PAGE_DESC_SIZE)
+            data = struct.unpack('<Q', page_descriptor)[0]
+            Numpages = data & 0b1111  # get the lower 4 bits
+            PageNum = data >> 4  # shift right 4 bits to get the upper 60 bits
+            page_count = (1+ Numpages)
+            total_page_count += page_count
+            self._segments.append(
+                (PageNum*self.PAGE_SIZE,
+                 mapped_address,
+                 self.PAGE_SIZE*page_count,
+                 size_of_compressed_data
+                )
+            )
+            for j in range(page_count):
+                self._mapping[(PageNum+j)*self.PAGE_SIZE] = position*self.PAGE_SIZE
+                position += 1
+
+        total_page_size = total_page_count*self.PAGE_SIZE
+        if total_page_size != size_of_compressed_data:
+            self._compressed[mapped_address] = huffman_compressed
+        return total_page_count, (4 + size_of_compressed_data + number_of_descs * self.PAGE_DESC_SIZE)  #Number of pages in the set, Size of the entire compression set
+
+
+    def _decode_data(
+        self, data: bytes, mapped_offset: int, offset: int, output_length: int
+    ) -> bytes:
+        start_offset, _, _, _ = self._find_segment(offset)
+        if mapped_offset in self._compressed:
+            try:
+                decoded_data = uncompress(data, self._compressed[mapped_offset])
+            except:
+                return bytearray(output_length)
+        else:
+            decoded_data = data
+        page_offset = self._mapping[start_offset]
+        decoded_data = decoded_data[page_offset + (offset - start_offset):]
+        decoded_data = decoded_data[:output_length]
+        if len(decoded_data) == output_length:
+            return decoded_data
+        else:
+            return bytearray(output_length)
+
+class HibernationFileStacker(interfaces.automagic.StackerLayerInterface):
+    stack_order = 10
+    @classmethod
+    def stack(
+        cls,
+        context: interfaces.context.ContextInterface,
+        layer_name: str,
+        progress_callback: constants.ProgressCallback = None,
+    ) -> Optional[interfaces.layers.DataLayerInterface]:
+        try:
+            HibernationLayer._check_header(context.layers[layer_name])
+        except exceptions.LayerException:
+            return None
+        new_name = context.layers.free_layer_name("HibernationLayer")
+        context.config[
+            interfaces.configuration.path_join(new_name, "base_layer")
+        ] = layer_name
+        layer = HibernationLayer(context, new_name, new_name)
+        cls.stacker_slow_warning()
+        return layer
