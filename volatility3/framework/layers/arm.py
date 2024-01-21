c9e7a3bc4e18b843993f482128c0cf58c095b9ef
Abyss Watcher <abyss-w4tcher@proton.me>
2024-01-21 13:14:26 +0100

    add volatility license

--- volatility3/framework/layers/arm.py
+++ volatility3/framework/layers/arm.py
@@ -1,3 +1,7 @@
+# This file is Copyright 2024 Volatility Foundation and licensed under the Volatility Software License 1.0
+# which is available at https://www.volatilityfoundation.org/license/vsl-v1.0
+#
+
 import logging
 import functools
 import collections
