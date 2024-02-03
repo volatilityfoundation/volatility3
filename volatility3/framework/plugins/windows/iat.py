cf9029fd85ae090034a253e3dcd819a0381b6442
k1nd0ne <k1nd0ne@mail.com>
2024-02-04 00:43:33 +0100

    Fixing the year in the Copyright

--- volatility3/framework/plugins/windows/iat.py
+++ volatility3/framework/plugins/windows/iat.py
@@ -1,4 +1,4 @@
-# This file is Copyright 2023 Volatility Foundation and licensed under the Volatility Software License 1.0
+# This file is Copyright 2024 Volatility Foundation and licensed under the Volatility Software License 1.0
 # which is available at https://www.volatilityfoundation.org/license/vsl-v1.0
 
 import logging, io, pefile
