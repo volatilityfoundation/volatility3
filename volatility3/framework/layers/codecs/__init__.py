9b855f1c29d7e826c85f18e2567dccce65242b3d
k1nd0ne <k1nd0ne@mail.com>
2024-07-23 14:32:27 +0200

    Deported the decompression algorithm in a dedicated python3 package

--- volatility3/framework/layers/codecs/__init__.py
+++ volatility3/framework/layers/codecs/__init__.py
@@ -2,9 +2,4 @@
 # which is available at https://www.volatilityfoundation.org/license/vsl-v1.0
 import codecs
 
-from volatility3.framework.layers.codecs.lz77plain import find_lz77_plain
-from volatility3.framework.layers.codecs.lz77huffman import find_lz77_huffman
-
-codecs.register(find_lz77_plain)
-codecs.register(find_lz77_huffman)
-# Register other codecs here.
+# Register codecs here.
