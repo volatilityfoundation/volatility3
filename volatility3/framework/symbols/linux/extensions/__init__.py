d964e6f61de00b5a8608d48d468d9a59b7923ec7
Gustavo Moreira <gmoreira@gmail.com>
2024-08-24 02:06:06 +1000

    PR review fixes: Check for LinuxUtilities version everywhere we use it

--- volatility3/framework/symbols/linux/extensions/__init__.py
+++ volatility3/framework/symbols/linux/extensions/__init__.py
@@ -13,6 +13,7 @@ from typing import Generator, Iterable, Iterator, Optional, Tuple, List, Union, 
 
 from volatility3.framework import constants, exceptions, objects, interfaces, symbols
 from volatility3.framework.renderers import conversion
+from volatility3.framework.configuration import requirements
 from volatility3.framework.constants.linux import SOCK_TYPES, SOCK_FAMILY
 from volatility3.framework.constants.linux import IP_PROTOCOLS, IPV6_PROTOCOLS
 from volatility3.framework.constants.linux import TCP_STATES, NETLINK_PROTOCOLS
@@ -1608,6 +1609,19 @@ class xdp_sock(objects.StructType):
 
 
 class bpf_prog(objects.StructType):
+    def _get_vmlinux(self):
+        linuxutils_required_version = (2, 1, 1)
+        linuxutils_current_version = linux.LinuxUtilities._version
+        if not requirements.VersionRequirement.matches_required(
+            linuxutils_required_version, linuxutils_current_version
+        ):
+            raise exceptions.PluginRequirementException(
+                f"linux.LinuxUtilities version not suitable: required {linuxutils_required_version} found {linuxutils_current_version}"
+            )
+
+        vmlinux = linux.LinuxUtilities.get_module_from_volobj_type(self._context, self)
+        return vmlinux
+
     def get_type(self) -> Union[str, None]:
         """Returns a string with the eBPF program type"""
 
@@ -1631,7 +1645,7 @@ class bpf_prog(objects.StructType):
         if not self.has_member("tag"):
             return None
 
-        vmlinux = linux.LinuxUtilities.get_module_from_volobj_type(self._context, self)
+        vmlinux = self._get_vmlinux()
         vmlinux_layer = vmlinux.context.layers[vmlinux.layer_name]
 
         prog_tag_addr = self.tag.vol.offset
@@ -2043,13 +2057,26 @@ class page(objects.StructType):
 
         return flags
 
+    def _get_vmlinux(self):
+        linuxutils_required_version = (2, 1, 1)
+        linuxutils_current_version = linux.LinuxUtilities._version
+        if not requirements.VersionRequirement.matches_required(
+            linuxutils_required_version, linuxutils_current_version
+        ):
+            raise exceptions.PluginRequirementException(
+                f"linux.LinuxUtilities version not suitable: required {linuxutils_required_version} found {linuxutils_current_version}"
+            )
+
+        vmlinux = linux.LinuxUtilities.get_module_from_volobj_type(self._context, self)
+        return vmlinux
+
     def to_paddr(self) -> int:
         """Converts a page's virtual address to its physical address using the current physical memory model.
 
         Returns:
             int: page physical address
         """
-        vmlinux = linux.LinuxUtilities.get_module_from_volobj_type(self._context, self)
+        vmlinux = self._get_vmlinux()
         vmlinux_layer = vmlinux.context.layers[vmlinux.layer_name]
 
         vmemmap_start = None
@@ -2100,7 +2127,7 @@ class page(objects.StructType):
         Returns:
             The page content
         """
-        vmlinux = linux.LinuxUtilities.get_module_from_volobj_type(self._context, self)
+        vmlinux = self._get_vmlinux()
         vmlinux_layer = vmlinux.context.layers[vmlinux.layer_name]
         physical_layer = vmlinux.context.layers["memory_layer"]
         page_paddr = self.to_paddr()
@@ -2117,6 +2144,19 @@ class IDR(objects.StructType):
     INT_SIZE = 4
     MAX_IDR_SHIFT = INT_SIZE * 8 - 1
     MAX_IDR_BIT = 1 << MAX_IDR_SHIFT
+
+    def _get_vmlinux(self):
+        linuxutils_required_version = (2, 1, 1)
+        linuxutils_current_version = linux.LinuxUtilities._version
+        if not requirements.VersionRequirement.matches_required(
+            linuxutils_required_version, linuxutils_current_version
+        ):
+            raise exceptions.PluginRequirementException(
+                f"linux.LinuxUtilities version not suitable: required {linuxutils_required_version} found {linuxutils_current_version}"
+            )
+
+        vmlinux = linux.LinuxUtilities.get_module_from_volobj_type(self._context, self)
+        return vmlinux
 
     def idr_max(self, num_layers: int) -> int:
         """Returns the maximum ID which can be allocated given idr::layers
@@ -2141,7 +2181,7 @@ class IDR(objects.StructType):
         Returns:
             A pointer to the given ID element
         """
-        vmlinux = linux.LinuxUtilities.get_module_from_volobj_type(self._context, self)
+        vmlinux = self._get_vmlinux()
         if not vmlinux.get_type("idr_layer").has_member("layer"):
             vollog.info(
                 "Unsupported IDR implementation, it should be a very very old kernel, probabably < 2.6"
