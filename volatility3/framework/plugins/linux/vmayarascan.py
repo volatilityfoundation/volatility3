6940af98ae2e736d4f6256f130e7a9b00269ce19
Andrew Case <andrew@dfir.org>
2024-10-24 15:11:52 -0500

    Add useful implementation of yara scanning to Linux

--- volatility3/framework/plugins/linux/vmayarascan.py
+++ volatility3/framework/plugins/linux/vmayarascan.py
@@ -2,7 +2,6 @@
 # which is available at https://www.volatilityfoundation.org/license/vsl-v1.0
 #
 
-import logging
 from typing import Iterable, List, Tuple
 
 from volatility3.framework import interfaces, renderers
@@ -11,14 +10,12 @@ from volatility3.framework.renderers import format_hints
 from volatility3.plugins import yarascan
 from volatility3.plugins.linux import pslist
 
-vollog = logging.getLogger(__name__)
-
 
 class VmaYaraScan(interfaces.plugins.PluginInterface):
     """Scans all virtual memory areas for tasks using yara."""
 
     _required_framework_version = (2, 22, 0)
-    _version = (1, 0, 4)
+    _version = (1, 0, 0)
 
     @classmethod
     def get_requirements(cls) -> List[interfaces.configuration.RequirementInterface]:
@@ -30,14 +27,14 @@ class VmaYaraScan(interfaces.plugins.PluginInterface):
                 description="Process IDs to include (all other processes are excluded)",
                 optional=True,
             ),
-            requirements.VersionRequirement(
-                name="pslist", component=pslist.PsList, version=(4, 0, 0)
+            requirements.PluginRequirement(
+                name="pslist", plugin=pslist.PsList, version=(4, 0, 1)
             ),
-            requirements.VersionRequirement(
-                name="yarascan", component=yarascan.YaraScan, version=(2, 0, 0)
+            requirements.PluginRequirement(
+                name="yarascan", plugin=yarascan.YaraScan, version=(2, 0, 1)
             ),
-            requirements.VersionRequirement(
-                name="yarascanner", component=yarascan.YaraScanner, version=(2, 0, 0)
+            requirements.PluginRequirement(
+                name="yarascanner", plugin=yarascan.YaraScanner, version=(2, 1, 1)
             ),
             requirements.ModuleRequirement(
                 name="kernel",
@@ -56,8 +53,6 @@ class VmaYaraScan(interfaces.plugins.PluginInterface):
         # use yarascan to parse the yara options provided and create the rules
         rules = yarascan.YaraScan.process_yara_options(dict(self.config))
 
-        sanity_check = 1024 * 1024 * 1024  # 1 GB
-
         # filter based on the pid option if provided
         filter_func = pslist.PsList.create_pid_filter(self.config.get("pid", None))
         for task in pslist.PsList.list_tasks(
@@ -74,46 +69,58 @@ class VmaYaraScan(interfaces.plugins.PluginInterface):
             # get the proc_layer object from the context
             proc_layer = self.context.layers[proc_layer_name]
 
-            max_vma_size = 0
-            vma_maps_to_scan = []
-            for start, size in self.get_vma_maps(task):
-                if size > sanity_check:
-                    vollog.debug(
-                        f"VMA at 0x{start:x} over sanity-check size, not scanning"
-                    )
-                    continue
-                max_vma_size = max(max_vma_size, size)
-                vma_maps_to_scan.append((start, size))
+            for start, end in self.get_vma_maps(task):
+                data = proc_layer.read(start, end - start, True)
+                if not yarascan.YaraScan._yara_x:
+                    for match in rules.match(data=data):
+                        if yarascan.YaraScan.yara_returns_instances():
+                            for match_string in match.strings:
+                                for instance in match_string.instances:
+                                    yield 0, (
+                                        format_hints.Hex(instance.offset + start),
+                                        task.tgid,
+                                        match.rule,
+                                        match_string.identifier,
+                                        renderers.LayerData(
+                                            self.context,
+                                            proc_layer_name,
+                                            instance.offset + start,
+                                            instance.matched_length,
+                                        ),
+                                    )
+                        else:
+                            for offset, name, value in match.strings:
+                                yield 0, (
+                                    format_hints.Hex(offset + start),
+                                    task.tgid,
+                                    match.rule,
+                                    name,
+                                    renderers.LayerData(
+                                        self.context,
+                                        proc_layer_name,
+                                        offset + start,
+                                        len(value),
+                                    ),
+                                )
+                else:
+                    for match in rules.scan(data).matching_rules:
+                        for match_string in match.patterns:
+                            for instance in match_string.matches:
+                                yield 0, (
+                                    format_hints.Hex(instance.offset + start),
+                                    task.tgid,
+                                    f"{match.namespace}.{match.identifier}",
+                                    match_string.identifier,
+                                    renderers.LayerData(
+                                        self.context,
+                                        proc_layer_name,
+                                        instance.offset + start,
+                                        instance.length,
+                                    ),
+                                )
 
-            if not vma_maps_to_scan:
-                vollog.warning(f"No VMAs were found for task {task.tgid}, not scanning")
-                continue
-
-            scanner = yarascan.YaraScanner(rules=rules)
-            scanner.chunk_size = max_vma_size
-
-            # scan the VMA data (in one contiguous block) with the yarascanner
-            for start, size in vma_maps_to_scan:
-                for offset, rule_name, name, value in scanner(
-                    proc_layer.read(start, size, pad=True), start
-                ):
-                    layer_data = renderers.LayerData(
-                        context=self.context,
-                        offset=offset,
-                        layer_name=proc_layer.name,
-                        length=len(value),
-                    )
-                    yield 0, (
-                        format_hints.Hex(offset),
-                        task.tgid,
-                        rule_name,
-                        name,
-                        layer_data,
-                    )
-
-    @classmethod
+    @staticmethod
     def get_vma_maps(
-        cls,
         task: interfaces.objects.ObjectInterface,
     ) -> Iterable[Tuple[int, int]]:
         """Creates a map of start/end addresses for each virtual memory area in a task.
