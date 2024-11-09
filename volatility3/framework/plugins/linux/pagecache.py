f71eb698731e1863448c50587ae83878df11a308
Joren Vrancken <jorenvrancken@gmail.com>
2024-11-09 15:16:25 +0100

    Remove unused import

--- volatility3/framework/plugins/linux/pagecache.py
+++ volatility3/framework/plugins/linux/pagecache.py
@@ -1,7 +1,6 @@
 # This file is Copyright 2024 Volatility Foundation and licensed under the Volatility Software License 1.0
 # which is available at https://www.volatilityfoundation.org/license/vsl-v1.0
 #
-import io
 import math
 import logging
 import datetime
