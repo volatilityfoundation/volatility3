971c31b0a88dd7fa8a9e51c5e4a9df17eb4b4352
Beyza Çoban <158472675+beyzacoban@users.noreply.github.com>
2026-08-24 14:16:23 +0300

    Expand process access category tests

--- test/plugins/windows/test_processaccess.py
+++ test/plugins/windows/test_processaccess.py
@@ -16,6 +16,35 @@ class TestWindowsProcessAccess:
     def test_decode_access_mask_none(self):
         assert processaccess.ProcessAccess.decode_access_mask(0) == "NONE"
 
+    def test_categorize_memory_read(self):
+        assert processaccess.ProcessAccess.categorize_access(0x0010) == "MEMORY_READ"
+
+    def test_categorize_memory_modification(self):
+        mask = 0x0008 | 0x0020
+        assert (
+            processaccess.ProcessAccess.categorize_access(mask)
+            == "MEMORY_MODIFICATION"
+        )
+
+    def test_categorize_thread_and_memory_modification(self):
+        mask = 0x0002 | 0x0008 | 0x0020
+        assert (
+            processaccess.ProcessAccess.categorize_access(mask)
+            == "THREAD_AND_MEMORY_MODIFICATION"
+        )
+
+    def test_categorize_combined_capabilities(self):
+        mask = 0x0010 | 0x0040 | 0x0800
+        assert processaccess.ProcessAccess.categorize_access(mask) == (
+            "MEMORY_READ,HANDLE_DUPLICATION,PROCESS_CONTROL"
+        )
+
+    def test_categorize_other_process_access(self):
+        assert (
+            processaccess.ProcessAccess.categorize_access(0x0400)
+            == "OTHER_PROCESS_ACCESS"
+        )
+
     def test_memory_related_access(self):
         assert processaccess.ProcessAccess.has_memory_related_access(0x0010)
         assert processaccess.ProcessAccess.has_memory_related_access(0x0020)
